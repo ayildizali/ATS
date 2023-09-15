87fdc5a4faa948d901e04e1c88e1649364945fd7
ayildizali <aymuhammedali00@gmail.com>
2023-09-15 12:01:28 +0300

    updata : App.vue

--- pages/VehicleInfo.vue
+++ pages/VehicleInfo.vue
@@ -1,3 +1,47 @@
+
 <template>
-  <div>car sdşljgkf</div>
+    <div class=" m-auto w-12/12 flex items-center justify-between">
+        <div class="flex-1">
+            <!-- header -->
+            <div class=" flex items-center justify-between w-9/12 h-32 m-auto  " >
+                <div class=" w-9/12 flex items-center ml-8  ">
+                        <img src="/public-src/Frame137.svg" class="mr-10">
+                        <h2 class="  leading-9 text-green-800 text-4xl ml-8 ">
+                            Araçlarım
+                        </h2>
+                        <h4 class=" text-base text-[#717982] ml-8"> / 34 HYD 1234 VOLKSWAGEN POLO </h4>
+                </div>
+
+                <div class="flex items-center">
+                    <nav class="pr-3">
+                      <img src="/public-src/Frame(12).svg">
+                    </nav>
+                    <p class="text-sm text-gray-500">Muhammed Ali Ay </p>
+                </div>
+            </div>
+            <!-- content-->
+            <div class="  w-9/12  h-32 flex items-center   m-auto  border-solid border-2 border-sky-500    ">
+                    <div class=" w-9/12  flex items-center  justify-between text-[#717982] m-auto">
+                        <p class="hover:text-[#1E724A]">ARAÇ</p>
+                        <p class="hover:text-[#1E724A]">KULLANICI BİLGİLERİ</p>
+                        <p class="hover:text-[#1E724A]">DİĞER BİLGİLER</p>
+                        <p class="hover:text-[#1E724A]">KM/HARCAMALAR</p>
+                        <p class="hover:text-[#1E724A]">BAKIM</p>
+                        <p class="hover:text-[#1E724A]">HGS/CEZA</p>
+                    </div>
+            </div>
+            <!-- main -->
+            <div class="">
+                <div class="w-5/12"></div>
+                <div class="w-5/12" ></div>
+            </div>
+        </div>
+
+    </div>
+
 </template>
+
+<script>
+
+
+</script>
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 
 <template>
-    <SideBar/>
+
   <router-view/>
 
 </template>
 
 <script setup lang="ts">
-import SideBar from "./components/SideBar.vue";
+
 </script>
 
 
