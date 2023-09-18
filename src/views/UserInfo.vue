926e01c447d316ab47a83edaef338cc206f1abac
ayildizali <aymuhammedali00@gmail.com>
2023-09-18 10:24:18 +0300

    new :UserInfo.vue

---
+++ src/views/UserInfo.vue
@@ -0,0 +1,159 @@
+<template>
+    <div class=" m-auto w-full flex items-center justify-between">
+        <div class="flex-1">
+            <!-- header -->
+            <div class="flex items-center justify-between w-3/4 h-32 m-auto" >
+                <div class="w-3/4 flex items-center ml-8">
+                    <img src="/public-src/Frame137.svg" class="mr-10">
+                    <h2 class="leading-9 text-green-800 text-4xl ml-8">
+                        Araçlarım
+                    </h2>
+                    <h4 class="text-base text-[#717982] ml-8"> / 34 HYD 1234 VOLKSWAGEN POLO </h4>
+                </div>
+
+                <div class="flex items-center">
+                    <nav class="pr-3">
+                        <img src="/public-src/Frame(12).svg">
+                    </nav>
+                    <p class="text-sm text-gray-500">Muhammed Ali Ay </p>
+                </div>
+            </div>
+            <!-- content-->
+            <div class="w-3/4 h-20 flex items-center m-auto border-solid border-2 border-primary rounded-xl">
+                <div class="w-3/4 flex items-center justify-between text-[#717982] m-auto">
+                    <h3 class="text-sm hover:text-primary">ARAÇ</h3>
+                    <h3 class="text-sm hover:text-primary">KULLANICI BİLGİLERİ</h3>
+                    <h3 class="text-sm hover:text-primary">DİĞER BİLGİLER</h3>
+                    <h3 class="text-sm hover:text-primary">KM/HARCAMALAR</h3>
+                    <h3 class="text-sm hover:text-primary">BAKIM</h3>
+                    <h3 class="text-sm hover:text-primary">HGS/CEZA</h3>
+                </div>
+            </div>
+            <!-- main -->
+            <div class="w-3/4 flex items-center justify-between m-auto mt-16">
+                <div class="w-full mt-0 ">
+                    <div  class="">
+                        <p class="text-Dark">Adı Soyadı</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="flex items-center ">
+                                <p class="pl-10 pt-2"> Kerem Baha Apuhan</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div  class="mt-6">
+                        <p class="text-Dark">T.C NO</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="flex items-center ">
+                                <p class="pl-10 pt-2"> 12345678910 </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div  class="mt-6">
+                        <p class="text-Dark">Telefon Bilgileri</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="flex items-center ">
+                                <p class="pl-10 pt-2"> 555 555 55 55</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div  class="mt-6">
+                        <p class="text-Dark">E-Mail Bilgileri</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="flex items-center ">
+                                <p class="pl-10 pt-2"> admin123@gmail.com </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div  class="mt-6">
+                        <p class="text-Dark"> İkamet Ettiği İl</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="flex items-center ">
+                                <p class="pl-10 pt-2"> Ankara </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-6">
+                        <p class="text-Dark">Taşıt Tanıma Durumu</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="flex items-center ">
+                                <p class="pl-10 pt-2"> Var </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-6">
+                        <p class="text-Dark">Taşıt Tanıma Limit Tutarı</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="flex items-center ">
+                                <p class="pl-10 pt-2">14.09.2023 </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-6">
+                        <p class="text-Dark">Araç Edinim Tarihi</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="flex items-center ">
+                                <p class="pl-10 pt-2">14.09.2023 </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="w-full" >
+                    <div class="">
+                        <p class="text-Dark">Bağlı Olduğu Birim Adı</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                            <p class="pl-10 pt-2">Genel Merkez</p>
+                        </div>
+                    </div>
+                    <div class="mt-6">
+                        <p class="text-Dark">Bulunduğu Yer (İl-İlçe)</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                            <p class="pl-10 pt-2">Genel Merkez - Ankara</p>
+                        </div>
+                    </div>
+
+                    <div class="mt-6">
+                        <p class="text-Dark">Proje Kodu</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                            <p class="pl-10 pt-2">2022-EMR-GEN-TR-102</p>
+                        </div>
+                    </div>
+
+                    <div class="mt-6">
+                        <p class="text-Dark">IRD Kodu</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                            <p class="pl-10 pt-2">IRD1025</p>
+                        </div>
+                    </div>
+                    <div class="mt-6">
+                        <p class="text-Dark">Trafik Sigortası, Bakım ve Diğer Giderleri Ödeme Mükellefi</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                            <p class="pl-10 pt-2">Dernek</p>
+                        </div>
+                    </div>
+                    <div class="mt-6">
+                        <p class="text-Dark">HGS-OGS-KGS Ödeme Mükellefi</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                            <p class="pl-10 pt-2">Dernek</p>
+                        </div>
+                    </div>
+                    <div class="mt-6">
+                        <p class="text-Dark">Trafik Cezası Ödeme Mükellefi</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                            <p class="pl-10 pt-2">Kendisi</p>
+                        </div>
+                    </div>
+                    <div class="mt-6">
+                        <p class="text-Dark">Önceki Kullanıcı</p>
+                        <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                            <p class="pl-10 pt-2">Muhammed Ali Ay</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+</script>
