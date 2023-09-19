00f353c08b0ed12f6ec496b49fab52faa1aa5606
ayildizali <aymuhammedali00@gmail.com>
2023-09-19 11:24:39 +0300

    updata:VehicleInfo.vue

---
+++ src/views/VehicleInfo.vue
@@ -0,0 +1,159 @@
+
+<template>
+
+    <div class=" m-auto w-full flex items-center justify-between">
+        <div class="flex-1">
+            <!-- header -->
+            <div class="flex items-center justify-between w-3/4 h-32 m-auto" >
+                <div class="w-3/4 flex items-center ml-8">
+                        <img src="/public-src/Frame137.svg" class="mr-10">
+                        <h2 class="leading-9 text-green-800 text-4xl ml-8 font-bold">
+                            Araçlarım
+                        </h2>
+                        <h4 class="text-base text-[#717982] ml-8"> / 34 HYD 1234 VOLKSWAGEN POLO </h4>
+                </div>
+
+                <div class="flex items-center">
+                    <nav class="pr-3">
+                      <img src="/public-src/Frame(12).svg">
+                    </nav>
+                    <p class="text-sm text-gray-500">Muhammed Ali Ay </p>
+                </div>
+            </div>
+            <!-- content-->
+            <div class="w-3/4 h-20 flex items-center m-auto border-solid border-2 border-primary rounded-xl">
+                    <div class="w-3/4 flex items-center justify-between text-[#717982] m-auto">
+                        <h3 class="text-sm hover:text-primary">ARAÇ</h3>
+                        <h3 class="text-sm hover:text-primary">KULLANICI BİLGİLERİ</h3>
+                        <h3 class="text-sm hover:text-primary">DİĞER BİLGİLER</h3>
+                        <h3 class="text-sm hover:text-primary">KM/HARCAMALAR</h3>
+                        <h3 class="text-sm hover:text-primary">BAKIM</h3>
+                        <h3 class="text-sm hover:text-primary">HGS/CEZA</h3>
+                    </div>
+            </div>
+            <!-- main -->
+            <div class="w-3/4 flex items-center justify-between ml-80 mt-16">
+                    <div class="w-full">
+                        <div  class="">
+                            <p class="text-Dark ">Plaka</p>
+                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                                <div class="flex items-center ">
+                                    <p class="pl-10 pt-2">  34 HYD 1234 </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div  class="mt-6">
+                            <p class="text-Dark">Araç Türü</p>
+                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                                <div class="flex items-center ">
+                                    <p class="pl-10 pt-2"> Otomobil </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div  class="mt-6">
+                            <p class="text-Dark">Yakıt Cinsi</p>
+                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                                <div class="flex items-center ">
+                                    <p class="pl-10 pt-2"> Benzinli</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div  class="mt-6">
+                            <p class="text-Dark">Ruhsat Sahibi</p>
+                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                                <div class="flex items-center ">
+                                    <p class="pl-10 pt-2">  HYİD </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div  class="mt-6">
+                            <p class="text-Dark">dont</p>
+                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                                <div class="flex items-center ">
+                                    <p class="pl-10 pt-2"> dont </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mt-6">
+                            <p class="text-Dark">Araç Alış Tarihi</p>
+                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                                <div class="flex items-center ">
+                                    <p class="pl-10 pt-2"> 13.09.2022 </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mt-6">
+                            <p class="text-Dark">KM Son Durum Tarihi</p>
+                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                                <div class="flex items-center ">
+                                    <p class="pl-10 pt-2">14.09.2023 </p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="w-full" >
+                        <div class="">
+                            <p class="text-Dark">Araç Markası</p>
+                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                                <p class="pl-10 pt-2">Volkswagen 1.4 Chrome Edition Polo</p>
+                            </div>
+                        </div>
+                        <div class="mt-6">
+                            <p class="text-Dark">Araç Model</p>
+                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                                <p class="pl-10 pt-2">2013</p>
+                            </div>
+                        </div>
+
+                        <div class="mt-6">
+                            <p class="text-Dark">Ruhsat Seri NO</p>
+                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                                <p class="pl-10 pt-2">GE-232425</p>
+                            </div>
+                        </div>
+
+                        <div class="mt-6">
+                            <p class="text-Dark">Ruhsat Örneği</p>
+                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                                <p class="pl-10 pt-2">https://www.resmigazete.gov.tr/2020/03/20200311-1-1.pdf</p>
+                            </div>
+                        </div>
+                        <div class="mt-6">
+                            <p class="text-Dark">Toplam Masraf</p>
+                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                                <p class="pl-10 pt-2">₺ 35.890 </p>
+                            </div>
+                        </div>
+                        <div class="mt-6">
+                            <p class="text-Dark">Araç Alış Bedeli</p>
+                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                                <p class="pl-10 pt-2"> ₺ 100.000,00</p>
+                            </div>
+                        </div>
+                        <div class="mt-6">
+                            <p class="text-Dark">KM Son Durum</p>
+                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                                <p class="pl-10 pt-2">104.000 KM</p>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+
+    </div>
+
+</template>
+<style scoped >
+
+</style>
+
+<script>
+
+
+import {defineComponent} from "vue";
+
+export default defineComponent({
+    components: {}
+})
+</script>
