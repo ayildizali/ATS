14a5175e6e3efc3e9eea4572fbc5d1ac49a14ccb
ayildizali <aymuhammedali00@gmail.com>
2023-09-19 11:24:12 +0300

    updata:index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import { createWebHistory, createRouter } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import VehicleInfo from "../views/VehicleInfo.vue";
 import UserInfo from "../views/UserInfo.vue";
-// import SideBar from "../components/SideBar.vue";
+import OtherInfo from "../views/OtherInfo.vue"
+import Repair from "../views/Repair.vue"
+import Expenses from "../views/Expenses.vue";
 
 
 const routes = [
@@ -20,8 +22,21 @@ const routes = [
         name:"UserInfo",
         component: UserInfo
     },
-
-
+    {
+        path:"/otherinfo",
+        name:"OtherInfo",
+        component: OtherInfo
+    },
+    {
+        path: "/repair",
+        name: "Repair",
+        component: Repair
+    },
+    {
+        path: "/expenses",
+        name:"Expenses",
+        component: Expenses
+    },
 
 
 
