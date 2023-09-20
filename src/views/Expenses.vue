116a5c318aaf5905de473bee6e34db132a8276c6
ayildizali <aymuhammedali00@gmail.com>
2023-09-20 13:58:41 +0300

    updata:Expenses.vue

--- src/views/Expenses.vue
+++ src/views/Expenses.vue
@@ -1,12 +1,141 @@
 <template>
- <div>
-     <div>
-         sldkfjg
-     </div>
- </div>
-</template>
-<style>
+    <div class="m-auto flex w-full items-center justify-between">
+        <div class="flex-1">
+            <!-- header -->
+            <div class="m-auto flex h-32 w-3/4 items-center justify-between" >
+                <div class="ml-8 flex w-3/4 items-center">
+                    <img src="/public-src/Frame137.svg" class="mr-10">
+                    <h2 class="ml-8 text-4xl font-bold leading-9 text-green-800">
+                        Araçlarım
+                    </h2>
+                    <h4 class="ml-8 text-base font-medium text-[#717982]"> / 34 HYD 1234 VOLKSWAGEN POLO </h4>
+                </div>
 
-</style>
-<script setup>
-</script>s
+                <div class="flex items-center">
+                    <nav class="pr-3">
+                        <img src="/public-src/Frame(12).svg">
+                    </nav>
+                    <p class="text-sm font-semibold text-gray-500">Muhammed Ali Ay </p>
+                </div>
+            </div>
+            <!-- content-->
+
+            <div class="m-auto flex h-20 w-3/4 items-center rounded-xl border-2 border-solid border-primary">
+                <div class="m-auto flex w-3/4 items-center justify-between text-[#717982]">
+                    <h3 class=" inline-block rounded-t-lg border-b-2 border-transparent font-bold hover:border-primary hover:text-primary">ARAÇ</h3>
+                    <h3 class="inline-block rounded-t-lg border-b-2 border-transparent font-bold hover:border-primary hover:text-primary">KULLANICI BİLGİLERİ</h3>
+                    <h3 class="inline-block rounded-t-lg border-b-2 border-transparent font-bold hover:border-primary hover:text-primary">DİĞER BİLGİLER</h3>
+                    <h3 class="inline-block rounded-t-lg border-b-2 border-transparent font-bold hover:border-primary hover:text-primary">KM/HARCAMALAR</h3>
+                    <h3 class="inline-block rounded-t-lg border-b-2 border-transparent font-bold hover:border-primary hover:text-primary">BAKIM</h3>
+                    <h3 class="inline-block rounded-t-lg border-b-2 border-transparent font-bold hover:border-primary hover:text-primary">HGS/CEZA</h3>
+                </div>
+            </div>
+
+            <!-- main -->
+            <div class="m-auto mt-12 flex  w-3/4  items-center">
+                <div class=" ml-16 w-full">
+                    <div class="mt-8 h-[500px] w-[480px] rounded-lg bg-GrayLightest shadow-xl ">
+                        <p class="mb-5 ml-8 pt-5 font-bold text-Dark900">ARAÇ KM BİLGİLERİ</p>
+                        <div class="flex items-center   ">
+                            <p class="h-16 w-52 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">9.08.2022</p>
+                            <p class="h-16 w-72 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">49.170 KM</p>
+                        </div>
+                        <div class="flex items-center   ">
+                            <p class="h-16 w-52 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">9.08.2022</p>
+                            <p class="h-16 w-72 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">49.170 KM</p>
+                        </div>
+                        <div class="flex items-center   ">
+                            <p class="h-16 w-52 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">9.08.2022</p>
+                            <p class="h-16 w-72 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">49.170 KM</p>
+                        </div>
+                        <div class="flex items-center   ">
+                            <p class="h-16 w-52 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">9.08.2022</p>
+                            <p class="h-16 w-72 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">49.170 KM</p>
+                        </div>
+                        <div class="flex items-center   ">
+                            <p class="h-16 w-52 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">9.08.2022</p>
+                            <p class="h-16 w-72 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">49.170 KM</p>
+                        </div>
+                        <div class="flex items-center   ">
+                            <p class="h-16 w-52 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">9.08.2022</p>
+                            <p class="h-16 w-72 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">49.170 KM</p>
+                        </div>
+                    </div>
+                    <div class="mt-12 flex w-5/6 items-center justify-end">
+                        <nav class="flex items-center  ">
+                            <a href="/public" class="pl-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chevron-left text-[#717982]" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                                </svg>
+                            </a>
+                           <img src="/public-src/Frame72.svg" class="pl-2">
+                            <a href="/public" class="pl-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chevron-right text-[#717982]" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </a>
+                        </nav>
+                    </div>
+
+                </div>
+                <div class="w-full">
+                    <div class="mt-8 h-[500px] w-[480px] rounded-lg bg-GrayLightest shadow-xl">
+                        <p class="ml-8 pt-5 font-bold text-Dark900">ARAÇ MASRAFLARI </p>
+<!--                        <p class="border-solid border-2 border-transparent border-b-Graydarker mt-5"></p>-->
+                        <div class=" mt-5 flex h-16 items-center justify-between border-y-2 border-Gray bg-Gray px-9 pt-2">
+                            <p class="text-sm font-bold text-Dark900">TOPLAM</p>
+                            <p></p>
+                            <p class="text-sm font-bold text-Dark900">₺24.000</p>
+                        </div>
+                        <div class="flex items-center   ">
+                            <p class="h-16 w-48 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">BAKIM</p>
+                            <p class="h-16 w-48 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">03.08.2022</p>
+                            <p class="h-16 w-48 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">₺1.000</p>
+                        </div>
+                        <div class="flex items-center   ">
+                            <p class="h-16 w-48 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">SERVİS</p>
+                            <p class="h-16 w-48 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">09.08.2022</p>
+                            <p class="h-16 w-48 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">₺4.000</p>
+                        </div>
+                        <div class="flex items-center   ">
+                            <p class="h-16 w-48 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">YAKIT</p>
+                            <p class="h-16 w-48 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">17.08.2022</p>
+                            <p class="h-16 w-48 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">₺3.000</p>
+                        </div>
+                        <div class="flex items-center   ">
+                            <p class="h-16 w-48 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">YAKIT</p>
+                            <p class="h-16 w-48 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900"></p>
+                            <p class="h-16 w-48 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">₺2.000</p>
+                        </div>
+                        <div class="flex items-center   ">
+                            <p class="h-16 w-48 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">YAKIT</p>
+                            <p class="h-16 w-48 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900"></p>
+                            <p class="h-16 w-48 border-2 border-Graydarker pl-9 pt-5 text-sm font-bold text-Dark900">₺6.000</p>
+                        </div>
+                    </div>
+                    <div class="mt-12 flex w-5/6 items-center justify-end">
+                        <nav class="flex items-center  ">
+                            <a href="/public" class="pl-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chevron-left text-[#717982]" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                                </svg>
+                            </a>
+                            <img src="/public-src/Frame72.svg" class="pl-2">
+                            <a href="/public" class="pl-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chevron-right text-[#717982]" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </a>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+
+</script>
