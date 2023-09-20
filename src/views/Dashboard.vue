8ec4d3e7bdbe7a4781e8b8006543a61185422fbc
ayildizali <aymuhammedali00@gmail.com>
2023-09-20 13:58:10 +0300

    updata:Dahsboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,10 @@
 
 <template>
-    <div class=" m-auto w-12/12 flex items-center justify-between">
+    <div class=" m-auto flex w-full items-center justify-between">
         <div class="flex-1">
             <!-- header -->
-            <div class=" flex items-center justify-between w-11/12 h-32 px-2 py4 m-auto text-4xl " >
-                <h2 class="  leading-9 text-green-800  ">
+            <div class="m-auto flex h-32 w-10/12 items-center justify-between px-2 py-4 text-4xl " >
+                <h2 class="  font-bold leading-9 text-green-800  ">
                     Araçlarım
                 </h2>
                 <div class="flex items-center">
@@ -16,27 +16,27 @@
                             </svg>
                         </a>
                     </nav>
-                    <p class="text-sm text-gray-500">Muhammed Ali Ay </p>
+                    <p class="text-sm font-semibold text-gray-500">Muhammed Ali Ay </p>
                 </div>
             </div>
 
             <!-- content-->
-            <div class="m-auto flex items-center justify-center p-8 text-cyan-700 w-10/12">
-                <div class=" flex items-center justify-between text-neutral-900 w-9/12">
+            <div class="m-auto flex w-10/12 items-center justify-center p-8 text-cyan-700">
+                <div class=" flex w-9/12 items-center justify-between text-neutral-900">
                     <p>Plaka</p>
                     <p>Araç Markası</p>
                     <p>Km</p>
                 </div>
 
             </div>
-            <div class=" m-auto mt-12 bg-gray-50 shadow rounded-2xl h-24 w-10/12">
-                <div class="p-8  flex items-center justify-between ">
+            <div class=" m-auto mt-12 h-24 w-10/12 rounded-2xl bg-gray-50 shadow">
+                <div class="flex  items-center justify-between p-8 ">
                     <a href="/public">
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-1-square-fill" viewBox="0 0 16 16">
                             <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm7.283 4.002V12H7.971V5.338h-.065L6.072 6.656V5.385l1.899-1.383h1.312Z"/>
                         </svg>
                     </a>
-                    <div class="flex items-center justify-between w-4/5">
+                    <div class="flex w-4/5 items-center justify-between">
                         <p>34 HYD 1234 </p>
                         <p> VOLKSWAGEN POLO</p>
                         <p>100.000</p>
@@ -50,12 +50,12 @@
 
                 </div>
             </div>
-            <div class=" m-auto mt-12 bg-gray-50 shadow rounded-2xl h-24 w-10/12">
-                <div class="p-8 flex items-center justify-between ">
+            <div class=" m-auto mt-12 h-24 w-10/12 rounded-2xl bg-gray-50 shadow">
+                <div class="flex items-center justify-between p-8 ">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-2-square-fill" viewBox="0 0 16 16">
                         <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm4.646 6.24v.07H5.375v-.064c0-1.213.879-2.402 2.637-2.402 1.582 0 2.613.949 2.613 2.215 0 1.002-.6 1.667-1.287 2.43l-.096.107-1.974 2.22v.077h3.498V12H5.422v-.832l2.97-3.293c.434-.475.903-1.008.903-1.705 0-.744-.557-1.236-1.313-1.236-.843 0-1.336.615-1.336 1.306Z"/>
                     </svg>
-                    <div class="flex items-center justify-between w-4/5">
+                    <div class="flex w-4/5 items-center justify-between">
                         <p>34 HYD 4321</p>
                         <p>RENAULT MEGAN</p>
                         <p>100.000</p>
@@ -69,14 +69,14 @@
 
                 </div>
             </div>
-            <div class=" m-auto mt-12 bg-gray-50 shadow rounded-2xl h-24 w-10/12">
-                <div class="p-8 flex items-center justify-between ">
+            <div class=" m-auto mt-12 h-24 w-10/12 rounded-2xl bg-gray-50 shadow">
+                <div class="flex items-center justify-between p-8 ">
                     <a href="/public">
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-3-square-fill" viewBox="0 0 16 16">
                             <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm5.918 8.414h-.879V7.342h.838c.78 0 1.348-.522 1.342-1.237 0-.709-.563-1.195-1.348-1.195-.79 0-1.312.498-1.348 1.055H5.275c.036-1.137.95-2.115 2.625-2.121 1.594-.012 2.608.885 2.637 2.062.023 1.137-.885 1.776-1.482 1.875v.07c.703.07 1.71.64 1.734 1.917.024 1.459-1.277 2.396-2.93 2.396-1.705 0-2.707-.967-2.754-2.144H6.33c.059.597.68 1.06 1.541 1.066.973.006 1.6-.563 1.588-1.354-.006-.779-.621-1.318-1.541-1.318Z"/>
                         </svg>
                     </a>
-                    <div class="flex items-center justify-between w-4/5">
+                    <div class="flex w-4/5 items-center justify-between">
                         <p>34 HYD 1234 </p>
                         <p>RENAULT MEGAN</p>
                         <p>100.000</p>
@@ -90,15 +90,15 @@
 
                 </div>
             </div>
-            <div class=" m-auto mt-12 bg-gray-50 shadow rounded-2xl h-24 w-10/12">
-                <div class="p-8  flex items-center justify-between ">
+            <div class=" m-auto mt-12 h-24 w-10/12 rounded-2xl bg-gray-50 shadow">
+                <div class="flex  items-center justify-between p-8 ">
                     <a href="/public">
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-4-square-fill" viewBox="0 0 16 16">
                             <path d="M6.225 9.281v.053H8.85V5.063h-.065c-.867 1.33-1.787 2.806-2.56 4.218Z"/>
                             <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm5.519 5.057c.22-.352.439-.703.657-1.055h1.933v5.332h1.008v1.107H10.11V12H8.85v-1.559H4.978V9.322c.77-1.427 1.656-2.847 2.542-4.265Z"/>
                         </svg>
                     </a>
-                    <div class="flex items-center justify-between w-4/5">
+                    <div class="flex w-4/5 items-center justify-between">
                         <p>34 HYD 1234 </p>
                         <p>RENAULT MEGAN</p>
                         <p>100.000</p>
@@ -112,14 +112,14 @@
 
                 </div>
             </div>
-            <div class=" m-auto mt-12 bg-gray-50 shadow rounded-2xl h-24 w-10/12">
-                <div class="p-8 flex items-center justify-between ">
+            <div class=" m-auto mt-12 h-24 w-10/12 rounded-2xl bg-gray-50 shadow">
+                <div class="flex items-center justify-between p-8 ">
                     <a href="/public">
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-5-square-fill" viewBox="0 0 16 16">
                             <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm5.994 12.158c-1.57 0-2.654-.902-2.719-2.115h1.237c.14.72.832 1.031 1.529 1.031.791 0 1.57-.597 1.57-1.681 0-.967-.732-1.57-1.582-1.57-.767 0-1.242.45-1.435.808H5.445L5.791 4h4.705v1.103H6.875l-.193 2.343h.064c.17-.258.715-.68 1.611-.68 1.383 0 2.561.944 2.561 2.585 0 1.687-1.184 2.806-2.924 2.806Z"/>
                         </svg>
                     </a>
-                    <div class="flex items-center justify-between w-4/5">
+                    <div class="flex w-4/5 items-center justify-between">
                         <p>34 HYD 1234 </p>
                         <p>RENAULT MEGAN</p>
                         <p>100.000</p>
@@ -133,15 +133,15 @@
 
                 </div>
             </div>
-            <div class=" m-auto mt-12  bg-gray-50 hover:bg-gray-50  shadow rounded-2xl h-24 w-10/12">
-                <div class="p-8 flex items-center justify-between  ">
+            <div class=" m-auto mt-12  h-24 w-10/12  rounded-2xl bg-gray-50 shadow hover:bg-gray-50">
+                <div class="flex items-center justify-between p-8  ">
                     <a href="/public">
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-6-square-fill" viewBox="0 0 16 16">
                             <path d="M8.111 7.863c-.92 0-1.564.65-1.564 1.576 0 1.032.703 1.635 1.558 1.635.868 0 1.553-.533 1.553-1.629 0-1.06-.744-1.582-1.547-1.582Z"/>
                             <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm6.21 3.855c1.612 0 2.515.99 2.573 1.899H9.494c-.1-.358-.51-.815-1.312-.815-1.078 0-1.817 1.09-1.805 3.036h.082c.229-.545.855-1.155 1.98-1.155 1.254 0 2.508.88 2.508 2.555 0 1.77-1.218 2.783-2.847 2.783-.932 0-1.84-.328-2.409-1.254-.369-.603-.597-1.459-.597-2.642 0-3.012 1.248-4.407 3.117-4.407Z"/>
                         </svg>
                     </a>
-                    <div class="flex items-center justify-between w-4/5">
+                    <div class="flex w-4/5 items-center justify-between">
                         <p>34 HYD 1234 </p>
                         <p>TOFAŞK</p>
                         <p>100.000</p>
@@ -155,7 +155,7 @@
 
                 </div>
             </div>
-            <div class="mt-32 m-auto flex items-center justify-between w-10/12 ">
+            <div class="m-auto mt-32 flex w-10/12 items-center justify-between ">
                 <div class="   ">
                     <p class="text-[#717982]">
                         50 sonuç bulundu.
@@ -169,7 +169,7 @@
                             </svg>
                         </a>
                         <a href="/public" class="pl-2" >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-1-square bg-[#1E724A] rounded  " viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-1-square rounded bg-[#1E724A]  " viewBox="0 0 16 16">
                                 <path d="M9.283 4.002V12H7.971V5.338h-.065L6.072 6.656V5.385l1.899-1.383h1.312Z"/>
                                 <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2Zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2Z"/>
                             </svg>
