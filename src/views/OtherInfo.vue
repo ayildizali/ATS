9de9cbae8c34feff0a74819ad31bdfe21746f59a
ayildizali <aymuhammedali00@gmail.com>
2023-09-20 14:07:13 +0300

    updata:OtherInfo.vue

--- src/views/OtherInfo.vue
+++ src/views/OtherInfo.vue
@@ -1,44 +1,44 @@
 <template>
 
-    <div class="m-auto w-full flex items-center justify-between">
+    <div class="m-auto flex w-full items-center justify-between">
         <div class="flex-1">
             <!-- header -->
-            <div class="flex items-center justify-between w-3/4 h-32 m-auto" >
-                <div class="w-3/4 flex items-center ml-8">
+            <div class="m-auto flex h-32 w-3/4 items-center justify-between" >
+                <div class="ml-8 flex w-3/4 items-center">
                     <img src="/public-src/Frame137.svg" class="mr-10">
-                    <h2 class="leading-9 text-green-800 text-4xl ml-8 font-bold">
+                    <h2 class="ml-8 text-4xl font-bold leading-9 text-green-800">
                         Araçlarım
                     </h2>
-                    <h4 class="text-base text-[#717982] ml-8"> / 34 HYD 1234 VOLKSWAGEN POLO </h4>
+                    <h4 class="ml-8 text-base font-medium text-[#717982]"> / 34 HYD 1234 VOLKSWAGEN POLO </h4>
                 </div>
 
                 <div class="flex items-center">
                     <nav class="pr-3">
                         <img src="/public-src/Frame(12).svg">
                     </nav>
-                    <p class="text-sm text-gray-500">Muhammed Ali Ay </p>
+                    <p class="text-sm font-semibold text-gray-500">Muhammed Ali Ay </p>
                 </div>
             </div>
             <!-- content-->
-            <div class="w-3/4 h-20 flex items-center m-auto border-solid border-2 border-primary rounded-xl">
-                <div class="w-3/4 flex items-center justify-between text-[#717982] m-auto">
-                    <h3 class="text-sm hover:text-primary">ARAÇ</h3>
-                    <h3 class="text-sm hover:text-primary">KULLANICI BİLGİLERİ</h3>
-                    <h3 class="text-sm hover:text-primary">DİĞER BİLGİLER</h3>
-                    <h3 class="text-sm hover:text-primary">KM/HARCAMALAR</h3>
-                    <h3 class="text-sm hover:text-primary">BAKIM</h3>
-                    <h3 class="text-sm hover:text-primary">HGS/CEZA</h3>
+            <div class="m-auto flex h-20 w-3/4 items-center rounded-xl border-2 border-solid border-primary">
+                <div class="m-auto flex w-3/4 items-center justify-between text-[#717982]">
+                    <h3 class=" inline-block rounded-t-lg border-b-2 border-transparent font-bold hover:border-primary hover:text-primary">ARAÇ</h3>
+                    <h3 class="inline-block rounded-t-lg border-b-2 border-transparent font-bold hover:border-primary hover:text-primary">KULLANICI BİLGİLERİ</h3>
+                    <h3 class="inline-block rounded-t-lg border-b-2 border-transparent font-bold hover:border-primary hover:text-primary">DİĞER BİLGİLER</h3>
+                    <h3 class="inline-block rounded-t-lg border-b-2 border-transparent font-bold hover:border-primary hover:text-primary">KM/HARCAMALAR</h3>
+                    <h3 class="inline-block rounded-t-lg border-b-2 border-transparent font-bold hover:border-primary hover:text-primary">BAKIM</h3>
+                    <h3 class="inline-block rounded-t-lg border-b-2 border-transparent font-bold hover:border-primary hover:text-primary">HGS/CEZA</h3>
                 </div>
             </div>
             <!-- main -->
-            <div class="w-3/4 flex items-center justify-between ml-80  mt-16 " >
-                <div class="w-full m-auto  ">
+            <div class="m-auto mt-16 flex w-3/4 items-center  justify-between " >
+                <div class="ml-16 w-full ">
                     <div class="mb-16">
-                        <h4 class="text-primary leading-5 font-bold flex items-center">MUAYENE BİLGİLERİ
+                        <h4 class="flex items-center font-bold leading-5 text-primary">MUAYENE BİLGİLERİ
                             <img src="/public-src/Line48.svg" class="pl-5"></h4>
                         <div  class="mb-6 mt-5">
                             <p class="text-Dark ">Yaklaşan Muayene Tarihi*</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
                                 <div class="flex items-center ">
                                     <p class="pl-10 pt-2">  34 HYD 1234 </p>
                                 </div>
@@ -46,7 +46,7 @@
                         </div>
                         <div  class="mt-6">
                             <p class="text-Dark">Son Muayene Tarihi*</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
                                 <div class="flex items-center ">
                                     <p class="pl-10 pt-2"> 19.05.2024 </p>
                                 </div>
@@ -54,7 +54,7 @@
                         </div>
                         <div  class="mt-6">
                             <p class="text-Dark">Muayene Ücreti</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
                                 <div class="flex items-center ">
                                     <p class="pl-10 pt-2">₺475,00</p>
                                 </div>
@@ -63,11 +63,11 @@
 
                     </div>
                     <div class="mt-14">
-                        <h4 class="text-primary leading-5 font-bold flex items-center">MTV BİLGİLERİ
+                        <h4 class="flex items-center font-bold leading-5 text-primary">MTV BİLGİLERİ
                             <img src="/public-src/Line48.svg" class="pl-5"></h4>
                         <div  class="mt-6">
                             <p class="text-Dark">Yaklaşan Ödeme Tarihi*</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
                                 <div class="flex items-center ">
                                     <p class="pl-10 pt-2">13.12.2023</p>
                                 </div>
@@ -75,7 +75,7 @@
                         </div>
                         <div  class="mt-6">
                             <p class="text-Dark"> Son Ödeme Tarihi*</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
                                 <div class="flex items-center ">
                                     <p class="pl-10 pt-2">13.09.2023</p>
                                 </div>
@@ -83,7 +83,7 @@
                         </div>
                         <div class="mt-6">
                             <p class="text-Dark">Son Ödenen Ücret</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
                                 <div class="flex items-center ">
                                     <p class="pl-10 pt-2"> ₺1.661,50</p>
                                 </div>
@@ -91,11 +91,11 @@
                         </div>
                     </div>
                     <div class="mt-14">
-                        <h4 class="text-primary leading-5 font-bold flex items-center">ARAÇ SERVİS BİLGİLERİ
+                        <h4 class="flex items-center font-bold leading-5 text-primary">ARAÇ SERVİS BİLGİLERİ
                         <img src="/public-src/Line48.svg" class="pl-5"></h4>
                         <div class="mt-6">
                             <p class="text-Dark">Servisler </p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
                                 <div class="flex items-center ">
                                     <p class="pl-10 pt-2"> Giray Yetkili Servis </p>
                                 </div>
@@ -103,7 +103,7 @@
                         </div>
                         <div class="mt-6">
                             <p class="text-Dark">Adres</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
                                 <div class="flex items-center ">
                                     <p class="pl-10 pt-2">Garyberli Mah. 12 Şubat İlçesi </p>
                                 </div>
@@ -111,7 +111,7 @@
                         </div>
                         <div class="mt-6">
                             <p class="text-Dark">Yetkili</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
                                 <div class="flex items-center ">
                                     <p class="pl-10 pt-2">Damla Hanım</p>
                                 </div>
@@ -119,7 +119,7 @@
                         </div>
                         <div class="mt-6">
                             <p class="text-Dark">Telefon</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl ">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
                                 <div class="flex items-center ">
                                     <p class="pl-10 pt-2">0344 236 16 25</p>
                                 </div>
@@ -129,73 +129,73 @@
                 </div>
                 <div class="w-full" >
                     <div class="mb-16">
-                        <h4 class="text-primary leading-5 font-bold flex items-center">SİGORTA BİLGİLERİ
+                        <h4 class="flex items-center font-bold leading-5 text-primary">SİGORTA BİLGİLERİ
                             <img src="/public-src/Line48.svg" class="pl-5"></h4>
                         <div class="mt-6">
                             <p class="text-Dark">Yaklaşan Sigorta Tarihi*</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
                                 <p class="pl-10 pt-2">13.09.2023</p>
                             </div>
                         </div>
                         <div class="mt-6">
                             <p class="text-Dark">Son Sigorta Tarihi*</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
                                 <p class="pl-10 pt-2">13.09.2023</p>
                             </div>
                         </div>
 
                         <div class="mt-6">
                             <p class="text-Dark">Sigorta Ücreti*</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
                                 <p class="pl-10 pt-2">₺2.427,02</p>
                             </div>
                         </div>
 
                         <div class="mt-6">
                             <p class="text-Dark">Trafik Sigortası*</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
-                                <p class="pl-10 pt-2">https://www.resmigazete.gov.tr/2020/03/20200311-1-1.pdf</p>
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
+                                <p class="pl-10 pt-2">https://www.resmigazete.gov.tr/pdf</p>
                             </div>
                         </div>
                         <div class="mt-6">
                             <p class="text-Dark">Kasko Sigortası*</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
-                                <p class="pl-10 pt-2">https://www.resmigazete.gov.tr/2020/03/20200311-1-1.pdf</p>
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
+                                <p class="pl-10 pt-2">https://www.resmigazete.gov.tr/.pdf</p>
                             </div>
                         </div>
                         <div class="mt-6">
                             <p class="text-Dark">Trafik Sigorta Adı*</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
                                 <p class="pl-10 pt-2">DOĞA SİGORTA</p>
                             </div>
                         </div>
                         <div class="mt-6">
                             <p class="text-Dark">Acenta Adı*</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
                                 <p class="pl-10 pt-2">104.000 KM</p>
                             </div>
                         </div>
                     </div>
                     <div class="">
-                        <h4 class="text-primary leading-5 font-bold flex items-center">GÜNCEL ARAÇ DEĞERİ
+                        <h4 class="flex items-center font-bold leading-5 text-primary">GÜNCEL ARAÇ DEĞERİ
                         <img src="/public-src/Line48.svg" class="pl-5"></h4>
                         <div class="mt-6">
                             <p class="text-Dark">Güncel Fiyat Bakılan Tarih</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
                                 <p class="pl-10 pt-2">13.09.2023</p>
                             </div>
                         </div>
                         <div class="mt-6">
                             <p class="text-Dark">Araç Güncel Değer</p>
-                            <div class="w-3/4 h-10 bg-GrayLighter rounded-xl">
+                            <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
                                 <p class="pl-10 pt-2"> ₺ 465.900</p>
                             </div>
                         </div>
 
                         <div class="mt-6">
                             <p class="text-Dark">Alınan Bilgi Linki</p>
-                            <div class="w-3/4 h-[135px] bg-GrayLighter rounded-xl">
-                                <p class="pl-10 pt-7">₺2.427,02https://www.sahibinden.com/ilan/vasita-otomobil-opel-2008-model-opel-corsa-1.3-cdti-otomatik-vites-sport-paket-1124414828/detay</p>
+                            <div class="h-[115px] w-3/4 rounded-xl bg-GrayLighter">
+                                <p class="pl-8 pt-5">₺2.427,02https://www.sahibinden.com/ilan/vasita-otomobil-opel-2008-model-opel-corsa-1.3-cdtit-1124414828/detay</p>
                             </div>
                         </div>
                     </div>
