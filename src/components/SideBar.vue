4fd00cba2ad2f9652f30421557d39a0ce4dfbe51
ayildizali <aymuhammedali00@gmail.com>
2023-09-20 17:26:32 +0300

    updata: SideBar.vue

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,54 +1,44 @@
 <template>
     <div class="relative flex min-h-screen  ">
-            <div class="bg-[#1E724A]  w-80 px-2 py-4 space-x-6 absolute inset-y-0 left-0 md:relative  md:-translate-x-0 transform -translate-x-full transition duration-200 ease-in-out " :class="{'relative translate-x-0':showsidebar}"  >
-                <img class="pt-7  m-auto" src="/public/public-src/HY_Logo Yatay 1.svg">
-                <div class="w-56 mt-10   ">
-                    <nav class="  flex items-center py-3 px-4 pl-12 pt-4 hover:bg-[#DBF5E9] rounded-lg text-sm">
+            <div class="absolute  inset-y-0 left-0 w-80 -translate-x-full space-x-6 bg-[#1E724A] px-2 py-4  transition duration-200 ease-in-out md:relative md:-translate-x-0" :class="{'relative translate-x-0':showsidebar}"  >
+                <img class="m-auto pt-7" src="/public/public-src/HY_Logo Yatay 1.svg">
+                <div class="mt-10 w-56">
+                    <nav class="  flex items-center rounded-lg px-4 py-3 pl-12 pt-4 text-sm hover:bg-[#DBF5E9]">
                         <img src="/public/public-src/Vector(7).svg" >
-                        <span class="text-white pl-4 hover:text-emerald-900" >Araçlarım</span>
+                        <span class="pl-4 text-white hover:text-emerald-900" >Araçlarım</span>
                     </nav>
-                    <nav class="flex items-center py-3 px-4 pl-12 pt-4 hover:bg-[#DBF5E9] rounded-lg text-sm" >
+                    <nav class="flex items-center rounded-lg px-4 py-3 pl-12 pt-4 text-sm hover:bg-[#DBF5E9]" >
                         <img src="/public/public-src/Vector(6).svg">
-                        <span class="text-white pl-4 hover:text-emerald-900">Araç Yönetimi</span>
+                        <span class="pl-4 text-white hover:text-emerald-900">Araç Yönetimi</span>
                     </nav>
-                    <nav class="flex items-center py-3 px-4 pl-12 pt-4 hover:bg-[#DBF5E9] rounded-lg text-sm">
+                    <nav class="flex items-center rounded-lg px-4 py-3 pl-12 pt-4 text-sm hover:bg-[#DBF5E9]">
                         <img src="/public/public-src/Vector(4).svg">
 
-                        <span class="text-white pl-4 hover:text-emerald-900">Ceza Ödeme</span>
+                        <span class="pl-4 text-white hover:text-emerald-900">Ceza Ödeme</span>
                     </nav>
-                    <nav class="flex items-center py-3 px-4 pl-12 pt-4 hover:bg-[#DBF5E9] transition-all rounded-lg text-sm">
+                    <nav class="flex items-center rounded-lg px-4 py-3 pl-12 pt-4 text-sm transition-all hover:bg-[#DBF5E9]">
                         <img src="/public/public-src/Frame(7).svg">
 
-                        <span class="text-white pl-4 hover:text-emerald-900">Periyodik Bakım</span>
+                        <span class="pl-4 text-white hover:text-emerald-900">Periyodik Bakım</span>
                     </nav>
-                    <nav class="flex items-center py-3 px-4 pl-12 pt-4 hover:bg-[#DBF5E9]   rounded-lg text-sm">
+                    <nav class="flex items-center rounded-lg px-4 py-3 pl-12 pt-4   text-sm hover:bg-[#DBF5E9]">
                         <a href="/public" class=" hover:text-emerald-900">
                           <img src="/public/public-src/Vector(5).svg">
                         </a>
-                        <span class="text-white pl-4 group-hover:text-emerald-900 ">Diğer Harcamalar</span>
+                        <span class="pl-4 text-white group-hover:text-emerald-900 ">Diğer Harcamalar</span>
                     </nav>
-                    <nav class="flex items-center py-3 px-4 pl-12 pt-4 hover:bg-[#DBF5E9]   rounded-lg text-sm">
+                    <nav class="flex items-center rounded-lg px-4 py-3 pl-12 pt-4   text-sm hover:bg-[#DBF5E9]">
                         <img src="/public/public-src/Frame(8).svg">
-                        <span class="text-white pl-4 hover:text-emerald-900">Kullanıcı Yönetimi</span>
+                        <span class="pl-4 text-white hover:text-emerald-900">Kullanıcı Yönetimi</span>
                     </nav>
                 </div>
-                <div class="  ml-8  w-56  absolute  bottom-10 flex items-center   ">
-                    <nav class=" pr-10 flex items-center  justify-center py-3 px-4 pl-12 hover:bg-[#DBF5E9]  hover:text-[#1E724A] rounded-lg text-sm ">
+                <div class="  absolute  bottom-10  ml-8  flex w-56 items-center   ">
+                    <nav class=" flex items-center justify-center  rounded-lg px-4 py-3 pl-12 pr-10  text-sm hover:bg-[#DBF5E9] hover:text-[#1E724A] ">
                        <img src="/public/public-src/Frame(10).svg">
-                        <span class="text-white pl-2 hover:text-emerald-900">Çıkış Yap</span>
+                        <span class="pl-2 text-white hover:text-emerald-900">Çıkış Yap</span>
                     </nav>
                 </div>
             </div>
-        <div class=" w-full bg-white shadow px-2 py-4">
-            <button
-                @click="showsidebar = !showsidebar"
-                class="text-[#1E724A] sm:hidden flex  justify-center ">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                </svg>
-                <img class="" src="/public/public-src/LogoYesil.svg">
-            </button>
-        </div>
     </div>
 </template>
 
