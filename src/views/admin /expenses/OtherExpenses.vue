b71becdd061125b446b7b4068cf7b8be24db30ef
ayildizali <aymuhammedali00@gmail.com>
2023-09-26 10:57:43 +0300

    new : OtherExpenses.vue

---
+++ src/views/admin /expenses/OtherExpenses.vue
@@ -0,0 +1,387 @@
+<template>
+  <div class=" flex h-full w-full items-center justify-between">
+    <div class="  flex-1">
+      <!-- header -->
+      <div class="m-auto flex h-32 w-3/4 items-center justify-between">
+        <div class="ml-5 flex w-3/4 items-center">
+          <img class="mr-10" src="/public-src/Kullanıcı/Frame137.svg">
+          <h2 class="ml-3 text-4xl font-bold leading-9 text-green-800">
+            Diğer Harcamalar
+          </h2>
+          <button
+              class="mb-2 ml-5 mr-2 mt-3 flex items-center rounded-lg bg-primary px-5 py-2.5 text-sm font-medium text-white ring-primary hover:bg-PrimaryLight hover:text-primary focus:outline-none focus:ring-2"
+              type="button">
+            <img class="mr-2 hover:text-primary" src="/public-src/Kullanıcı/Frame(13).svg">
+            Yeni İşlem
+          </button>
+        </div>
+
+        <div class="flex items-center">
+          <nav class="pr-3">
+            <img src="/public-src/Kullanıcı/Frame(12).svg">
+          </nav>
+          <p class="text-sm font-semibold text-gray-500">Muhammed Ali Ay </p>
+        </div>
+      </div>
+      <!-- content-->
+
+      <div class=" ">
+      </div>
+      <div class="m-auto flex w-9/12 justify-end">
+        <div class="mt-16 h-36  w-64 rounded-lg border-2 border-solid border-b-Graydarker  bg-GrayLightest">
+          <div class=" ">
+            <p class="ml-8 mt-7 flex items-center font-bold text-Dark900">TOPLAM TUTAR</p>
+            <p class=" mx-3 mt-5 flex items-center border-b-2 border-b-Graydarker"></p>
+            <p class="ml-8 mt-5 flex items-center font-bold text-Dark900 ">₺56.774,00</p>
+          </div>
+        </div>
+      </div>
+      <!-- main -->
+      <div class="relative  m-auto mt-16  flex w-3/4 items-center overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-left text-sm text-GrayLightest">
+          <thead class="bg-GrayLightest text-xs uppercase text-Dark900">
+          <tr>
+            <th class="px-6 py-3" scope="col">
+
+            </th>
+            <th class="px-6 py-3" scope="col">
+              Araç Kullanıcısı
+            </th>
+            <th class="px-6 py-3" scope="col">
+              tarİh
+            </th>
+            <th class="px-6 py-3" scope="col">
+              fatura
+            </th>
+            <th class="px-6 py-3" scope="col">
+              plaka
+            </th>
+            <th class="px-6 py-3" scope="col">
+              harcama türü
+            </th>
+            <th class="px-6 py-3" scope="col">
+              il
+            </th>
+            <th class="px-6 py-3" scope="col">
+              fatura tutarı
+            </th>
+            <th class="px-6 py-3" scope="col">
+              açıklama
+            </th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr class="border-b bg-white ">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
+              <img src="/public-src/admin/Frame(17).svg">
+            </th>
+            <td class="px-6 py-4 text-Dark900">
+              Muhammet Mustafa Yaprak
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              24.03.2023
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              Fatura
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              34 HYD 1234
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              yakıt
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              gaziantep
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              ₺200.000,00
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              4 Adet Lastik Alımı
+            </td>
+          </tr>
+          <tr class="border-b bg-gray-50 text-Graydarker">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
+              <img src="/public-src/admin/Frame(17).svg">
+            </th>
+            <td class="px-6 py-4 text-Dark900">
+              Muhammet Mustafa Yaprak
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              24.03.2023
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              Fatura
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              34 HYD 1234
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              yakıt
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              gaziantep
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              ₺200.000,00
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              4 Adet Lastik Alımı
+            </td>
+          </tr>
+          <tr class="border-b bg-white ">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
+              <img src="/public-src/admin/Frame(17).svg">
+            </th>
+            <td class="px-6 py-4 text-Dark900">
+              Muhammet Mustafa Yaprak
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              24.03.2023
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              Fatura
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              34 HYD 1234
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              yakıt
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              gaziantep
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              ₺200.000,00
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              4 Adet Lastik Alımı
+            </td>
+          </tr>
+          <tr class="border-b bg-gray-50 text-Graydarker">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
+              <img src="/public-src/admin/Frame(17).svg">
+            </th>
+            <td class="px-6 py-4 text-Dark900">
+              Muhammet Mustafa Yaprak
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              24.03.2023
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              Fatura
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              34 HYD 1234
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              yakıt
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              gaziantep
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              ₺200.000,00
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              4 Adet Lastik Alımı
+            </td>
+          </tr>
+          <tr class="border-b bg-white ">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
+              <img src="/public-src/admin/Frame(17).svg">
+            </th>
+            <td class="px-6 py-4 text-Dark900">
+              Muhammet Mustafa Yaprak
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              24.03.2023
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              Fatura
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              34 HYD 1234
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              yakıt
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              gaziantep
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              ₺200.000,00
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              4 Adet Lastik Alımı
+            </td>
+          </tr>
+          <tr class="border-b bg-gray-50 text-Graydarker">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
+              <img src="/public-src/admin/Frame(17).svg">
+            </th>
+            <td class="px-6 py-4 text-Dark900">
+              Muhammet Mustafa Yaprak
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              24.03.2023
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              Fatura
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              34 HYD 1234
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              yakıt
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              gaziantep
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              ₺200.000,00
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              4 Adet Lastik Alımı
+            </td>
+          </tr>
+          <tr class="border-b bg-white ">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
+              <img src="/public-src/admin/Frame(17).svg">
+            </th>
+            <td class="px-6 py-4 text-Dark900">
+              Muhammet Mustafa Yaprak
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              24.03.2023
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              Fatura
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              34 HYD 1234
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              yakıt
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              gaziantep
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              ₺200.000,00
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              4 Adet Lastik Alımı
+            </td>
+          </tr>
+          <tr class="border-b bg-gray-50 text-Graydarker">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
+              <img src="/public-src/admin/Frame(17).svg">
+            </th>
+            <td class="px-6 py-4 text-Dark900">
+              Muhammet Mustafa Yaprak
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              24.03.2023
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              Fatura
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              34 HYD 1234
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              yakıt
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              gaziantep
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              ₺200.000,00
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              4 Adet Lastik Alımı
+            </td>
+          </tr>
+          <tr class="border-b bg-white ">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
+              <img src="/public-src/admin/Frame(17).svg">
+            </th>
+            <td class="px-6 py-4 text-Dark900">
+              Muhammet Mustafa Yaprak
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              24.03.2023
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              Fatura
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              34 HYD 1234
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              yakıt
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              gaziantep
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              ₺200.000,00
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              4 Adet Lastik Alımı
+            </td>
+          </tr>
+          <tr class="border-b bg-gray-50 text-Graydarker">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
+              <img src="/public-src/admin/Frame(17).svg">
+            </th>
+            <td class="px-6 py-4 text-Dark900">
+              Muhammet Mustafa Yaprak
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              24.03.2023
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              Fatura
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              34 HYD 1234
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              yakıt
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              gaziantep
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              ₺200.000,00
+            </td>
+            <td class="px-6 py-4 text-Dark900">
+              4 Adet Lastik Alımı
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <div class="mt-14 flex w-11/12 items-center justify-end">
+          <div class="flex items-center justify-end">
+            <img class="pl-5" src="/public-src/admin/rightRounded.svg">
+            <img class="pl-5" src="/public-src/admin/Frame72.svg">
+            <img class="pl-5" src="/public-src/admin/LeftRounded.svg">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "OtherExpeneses",
+  components: {}
+})
+</script>
