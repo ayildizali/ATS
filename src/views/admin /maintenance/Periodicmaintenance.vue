8755d09e3e2c33566e4c95e90d4d3430232a87a4
ayildizali <aymuhammedali00@gmail.com>
2023-09-27 11:08:48 +0300

    updata : Periodicmaintenance.vue

--- src/views/admin /maintenance/Periodicmaintenance.vue
+++ src/views/admin /maintenance/Periodicmaintenance.vue
@@ -2,14 +2,16 @@
   <div class=" flex h-full w-full items-center justify-between">
     <div class="  flex-1">
       <!-- header -->
-      <div class="m-auto flex h-32 w-3/4 items-center justify-between" >
+      <div class="m-auto flex h-32 w-3/4 items-center justify-between">
         <div class="ml-5 flex w-3/4 items-center">
-          <img src="/public-src/Kullanıcı/Frame137.svg" class="mr-10">
-          <h2 class="ml-3 text-4xl font-bold leading-9 text-green-800">
+          <img class="mr-5" src="/public-src/Kullanıcı/Frame137.svg">
+          <h2 class="text-4xl font-bold leading-9 text-green-800">
             Periyodik Bakım
           </h2>
-          <button  type="button" class="mb-2 ml-5 mr-2 mt-3 flex items-center rounded-lg bg-primary px-5 py-2.5 text-sm font-medium text-white ring-primary hover:bg-PrimaryLight hover:text-primary focus:outline-none focus:ring-2">
-            <img src="/public-src/Kullanıcı/Frame(13).svg" class="mr-2 hover:text-primary">
+          <button
+              class="mb-2 ml-5 mr-2 mt-3 flex items-center rounded-lg bg-primary px-5 py-2.5 text-sm font-medium text-white ring-primary hover:bg-PrimaryLight hover:text-primary focus:outline-none focus:ring-2"
+              type="button">
+            <img class="mr-2 hover:text-primary" src="/public-src/Kullanıcı/Frame(13).svg">
             Yeni ceza
           </button>
         </div>
@@ -28,7 +30,7 @@
       <div class="m-auto flex w-9/12 justify-end">
         <div class="mt-16 h-36  w-64 rounded-lg border-2 border-solid border-b-Graydarker  bg-GrayLightest">
           <div class=" ">
-            <p class="ml-8 mt-7 flex items-center font-bold text-Dark900" >TOPLAM TUTAR</p>
+            <p class="ml-8 mt-7 flex items-center font-bold text-Dark900">TOPLAM TUTAR</p>
             <p class=" mx-3 mt-5 flex items-center border-b-2 border-b-Graydarker"></p>
             <p class="ml-8 mt-5 flex items-center font-bold text-Dark900 ">₺56.774,00</p>
           </div>
@@ -39,125 +41,47 @@
         <table class="w-full text-left text-sm text-GrayLightest">
           <thead class="bg-GrayLightest text-xs uppercase text-Dark900">
           <tr>
-            <th scope="col" class="px-6 py-3">
+            <th class="px-6 py-3" scope="col">
 
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th class="px-6 py-3" scope="col">
               Araç Kullanıcısı
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th class="px-6 py-3" scope="col">
               tarİh
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th class="px-6 py-3" scope="col">
               fatura
             </th>
-            <th scope="col" class="px-6 py-3">
-             fatura ünvanı
+            <th class="px-6 py-3" scope="col">
+              fatura ünvanı
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th class="px-6 py-3" scope="col">
               plaka
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th class="px-6 py-3" scope="col">
               il
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th class="px-6 py-3" scope="col">
               fatura tutarı
             </th>
-            <th scope="col" class="px-6 py-3">
-             km
+            <th class="px-6 py-3" scope="col">
+              km
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th class="px-6 py-3" scope="col">
               oto lastik
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th class="px-6 py-3" scope="col">
               bakım türü
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th class="px-6 py-3" scope="col">
               açıklama
             </th>
           </tr>
           </thead>
           <tbody>
           <tr class="border-b bg-white ">
-            <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-Dark900">
-              <img src="/public-src/admin/Frame(17).svg">
-            </th>
-            <td class="px-6 py-4 text-Dark900">
-             Muhammet Mustafa Yaprak
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              24.03.2023
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Fatura
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Fatura Öz Asm Oto
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              34 HYD 1234
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              gaziantep
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              ₺200.000,00
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              150.000 KM
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              205/55 R 16 (Goodyear)
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Araç Motor İç & Dış
-              Diğer Temizlik
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              4 Adet Lastik Alımı
-            </td>
-          </tr>
-          <tr class="border-b bg-gray-50 text-Graydarker">
-            <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-Dark900">
-              <img src="/public-src/admin/Frame(17).svg">
-            </th>
-            <td class="px-6 py-4 text-Dark900">
-              Muhammet Mustafa Yaprak
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              24.03.2023
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Fatura
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Fatura Öz Asm Oto
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              34 HYD 1234
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              gaziantep
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              ₺200.000,00
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              150.000 KM
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              205/55 R 16 (Goodyear)
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Araç Motor İç & Dış
-              Diğer Temizlik
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              4 Adet Lastik Alımı
-            </td>
-          </tr>
-          <tr class="border-b bg-white ">
-            <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-Dark900">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
               <img src="/public-src/admin/Frame(17).svg">
             </th>
             <td class="px-6 py-4 text-Dark900">
@@ -196,7 +120,7 @@
             </td>
           </tr>
           <tr class="border-b bg-gray-50 text-Graydarker">
-            <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-Dark900">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
               <img src="/public-src/admin/Frame(17).svg">
             </th>
             <td class="px-6 py-4 text-Dark900">
@@ -235,7 +159,7 @@
             </td>
           </tr>
           <tr class="border-b bg-white ">
-            <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-Dark900">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
               <img src="/public-src/admin/Frame(17).svg">
             </th>
             <td class="px-6 py-4 text-Dark900">
@@ -274,7 +198,7 @@
             </td>
           </tr>
           <tr class="border-b bg-gray-50 text-Graydarker">
-            <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-Dark900">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
               <img src="/public-src/admin/Frame(17).svg">
             </th>
             <td class="px-6 py-4 text-Dark900">
@@ -313,7 +237,7 @@
             </td>
           </tr>
           <tr class="border-b bg-white ">
-            <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-Dark900">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
               <img src="/public-src/admin/Frame(17).svg">
             </th>
             <td class="px-6 py-4 text-Dark900">
@@ -352,7 +276,7 @@
             </td>
           </tr>
           <tr class="border-b bg-gray-50 text-Graydarker">
-            <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-Dark900">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
               <img src="/public-src/admin/Frame(17).svg">
             </th>
             <td class="px-6 py-4 text-Dark900">
@@ -391,163 +315,7 @@
             </td>
           </tr>
           <tr class="border-b bg-white ">
-            <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-Dark900">
-              <img src="/public-src/admin/Frame(17).svg">
-            </th>
-            <td class="px-6 py-4 text-Dark900">
-              Muhammet Mustafa Yaprak
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              24.03.2023
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Fatura
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Fatura Öz Asm Oto
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              34 HYD 1234
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              gaziantep
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              ₺200.000,00
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              150.000 KM
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              205/55 R 16 (Goodyear)
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Araç Motor İç & Dış
-              Diğer Temizlik
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              4 Adet Lastik Alımı
-            </td>
-          </tr>
-          <tr class="border-b bg-gray-50 text-Graydarker">
-            <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-Dark900">
-              <img src="/public-src/admin/Frame(17).svg">
-            </th>
-            <td class="px-6 py-4 text-Dark900">
-              Muhammet Mustafa Yaprak
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              24.03.2023
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Fatura
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Fatura Öz Asm Oto
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              34 HYD 1234
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              gaziantep
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              ₺200.000,00
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              150.000 KM
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              205/55 R 16 (Goodyear)
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Araç Motor İç & Dış
-              Diğer Temizlik
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              4 Adet Lastik Alımı
-            </td>
-          </tr>
-          <tr class="border-b bg-white ">
-            <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-Dark900">
-              <img src="/public-src/admin/Frame(17).svg">
-            </th>
-            <td class="px-6 py-4 text-Dark900">
-              Muhammet Mustafa Yaprak
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              24.03.2023
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Fatura
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Fatura Öz Asm Oto
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              34 HYD 1234
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              gaziantep
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              ₺200.000,00
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              150.000 KM
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              205/55 R 16 (Goodyear)
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Araç Motor İç & Dış
-              Diğer Temizlik
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              4 Adet Lastik Alımı
-            </td>
-          </tr>
-          <tr class="border-b bg-gray-50 text-Graydarker">
-            <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-Dark900">
-              <img src="/public-src/admin/Frame(17).svg">
-            </th>
-            <td class="px-6 py-4 text-Dark900">
-              Muhammet Mustafa Yaprak
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              24.03.2023
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Fatura
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Fatura Öz Asm Oto
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              34 HYD 1234
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              gaziantep
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              ₺200.000,00
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              150.000 KM
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              205/55 R 16 (Goodyear)
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              Araç Motor İç & Dış
-              Diğer Temizlik
-            </td>
-            <td class="px-6 py-4 text-Dark900">
-              4 Adet Lastik Alımı
-            </td>
-          </tr>
-          <tr class="border-b bg-white ">
-            <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-Dark900">
+            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
               <img src="/public-src/admin/Frame(17).svg">
             </th>
             <td class="px-6 py-4 text-Dark900">
@@ -590,10 +358,10 @@
       </div>
       <div>
         <div class="mt-14 flex w-11/12 items-center justify-end">
-          <div class="flex items-center justify-end" >
-            <img src="/public-src/admin/rightRounded.svg" class="pl-5" >
-            <img src="/public-src/admin/Frame72.svg" class="pl-5">
-            <img src="/public-src/admin/LeftRounded.svg" class="pl-5">
+          <div class="flex items-center justify-end">
+            <img class="pl-5" src="/public-src/admin/rightRounded.svg">
+            <img class="pl-5" src="/public-src/admin/Frame72.svg">
+            <img class="pl-5" src="/public-src/admin/LeftRounded.svg">
           </div>
         </div>
       </div>
