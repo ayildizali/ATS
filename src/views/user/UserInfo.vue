dec63781e3ac629852fc0189891981b6172edd71
ayildizali <aymuhammedali00@gmail.com>
2023-09-27 15:47:36 +0300

    updata : UserInfo.vue

--- src/views/user/UserInfo.vue
+++ src/views/user/UserInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class=" m-auto flex h-full w-full items-center justify-between">
+  <div class=" m-auto mt-0 flex h-full w-full items-center justify-between">
     <div class="flex-1">
       <!-- header -->
       <div class="m-auto flex h-32 w-3/4 items-center justify-between">
@@ -20,7 +20,7 @@
       </div>
       <!-- content-->
       <div class="m-auto flex h-20 w-3/4 items-center rounded-xl border-2 border-solid border-primary">
-        <div class="m-auto flex w-3/4 items-center justify-between text-[#717982]">
+        <div class="m-auto flex w-3/4 items-center justify-between text-Dark800">
           <RouterLink
               class="inline-block rounded-t-lg border-b-2 border-transparent font-bold hover:border-primary hover:text-primary"
               to="/vehicle">ARAÇLARIM
@@ -48,10 +48,10 @@
         </div>
       </div>
       <!-- main -->
-      <div class="m-60 mt-16 flex w-3/4 items-center justify-between">
-        <div class="ml-32 w-full">
+      <div class="m-auto mt-16 flex w-3/4 items-center justify-between">
+        <div class="ml-20 w-full">
           <div class=" ">
-            <p class="text-Dark">Adı Soyadı</p>
+            <p class="pl-5 text-Dark">Adı Soyadı</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
               <div class="flex items-center ">
                 <p class="pl-10 pt-2"> Kerem Baha Apuhan</p>
@@ -59,7 +59,7 @@
             </div>
           </div>
           <div class="mt-6">
-            <p class="text-Dark">T.C NO</p>
+            <p class="pl-5 text-Dark">T.C NO</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
               <div class="flex items-center ">
                 <p class="pl-10 pt-2"> 12345678910 </p>
@@ -67,7 +67,7 @@
             </div>
           </div>
           <div class="mt-6">
-            <p class="text-Dark">Telefon Bilgileri</p>
+            <p class="pl-5 text-Dark">Telefon Bilgileri</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
               <div class="flex items-center ">
                 <p class="pl-10 pt-2"> 555 555 55 55</p>
@@ -75,7 +75,7 @@
             </div>
           </div>
           <div class="mt-6">
-            <p class="text-Dark">E-Mail Bilgileri</p>
+            <p class="pl-5 text-Dark">E-Mail Bilgileri</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
               <div class="flex items-center ">
                 <p class="pl-10 pt-2"> admin123@gmail.com </p>
@@ -83,7 +83,7 @@
             </div>
           </div>
           <div class="mt-6">
-            <p class="text-Dark"> İkamet Ettiği İl</p>
+            <p class="pl-5 text-Dark"> İkamet Ettiği İl</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
               <div class="flex items-center ">
                 <p class="pl-10 pt-2"> Ankara </p>
@@ -91,7 +91,7 @@
             </div>
           </div>
           <div class="mt-6">
-            <p class="text-Dark">Taşıt Tanıma Durumu</p>
+            <p class="pl-5 text-Dark">Taşıt Tanıma Durumu</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
               <div class="flex items-center ">
                 <p class="pl-10 pt-2"> Var </p>
@@ -99,7 +99,7 @@
             </div>
           </div>
           <div class="mt-6">
-            <p class="text-Dark">Taşıt Tanıma Limit Tutarı</p>
+            <p class="pl-5 text-Dark">Taşıt Tanıma Limit Tutarı</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
               <div class="flex items-center ">
                 <p class="pl-10 pt-2">14.09.2023 </p>
@@ -107,7 +107,7 @@
             </div>
           </div>
           <div class="mt-6">
-            <p class="text-Dark">Araç Edinim Tarihi</p>
+            <p class="pl-5 text-Dark">Araç Edinim Tarihi</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
               <div class="flex items-center ">
                 <p class="pl-10 pt-2">14.09.2023 </p>
@@ -118,51 +118,51 @@
         </div>
         <div class="w-full ">
           <div class="">
-            <p class="text-Dark">Bağlı Olduğu Birim Adı</p>
+            <p class="pl-5 text-Dark">Bağlı Olduğu Birim Adı</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
               <p class="pl-10 pt-2">Genel Merkez</p>
             </div>
           </div>
           <div class="mt-6">
-            <p class="text-Dark">Bulunduğu Yer (İl-İlçe)</p>
+            <p class="pl-5 text-Dark">Bulunduğu Yer (İl-İlçe)</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
               <p class="pl-10 pt-2">Genel Merkez - Ankara</p>
             </div>
           </div>
 
           <div class="mt-6">
-            <p class="text-Dark">Proje Kodu</p>
+            <p class="pl-5 text-Dark">Proje Kodu</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
               <p class="pl-10 pt-2">2022-EMR-GEN-TR-102</p>
             </div>
           </div>
 
           <div class="mt-6">
-            <p class="text-Dark">IRD Kodu</p>
+            <p class="pl-5 text-Dark">IRD Kodu</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
               <p class="pl-10 pt-2">IRD1025</p>
             </div>
           </div>
           <div class="mt-6">
-            <p class="text-Dark">Trafik Sigortası, Bakım ve Diğer Giderleri Ödeme Mükellefi</p>
+            <p class="pl-5 text-Dark">Trafik Sigortası, Bakım ve Diğer Giderleri Ödeme Mükellefi</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
               <p class="pl-10 pt-2">Dernek</p>
             </div>
           </div>
           <div class="mt-6">
-            <p class="text-Dark">HGS-OGS-KGS Ödeme Mükellefi</p>
+            <p class="pl-5 text-Dark">HGS-OGS-KGS Ödeme Mükellefi</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
               <p class="pl-10 pt-2">Dernek</p>
             </div>
           </div>
           <div class="mt-6">
-            <p class="text-Dark">Trafik Cezası Ödeme Mükellefi</p>
+            <p class="pl-5 text-Dark">Trafik Cezası Ödeme Mükellefi</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
               <p class="pl-10 pt-2">Kendisi</p>
             </div>
           </div>
           <div class="mt-6">
-            <p class="text-Dark">Önceki Kullanıcı</p>
+            <p class="pl-5 text-Dark">Önceki Kullanıcı</p>
             <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
               <p class="pl-10 pt-2">Muhammed Ali Ay</p>
             </div>
