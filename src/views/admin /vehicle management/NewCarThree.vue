4e3e014f7ffa3cb4a3f04c77246e94457b9f0382
ayildizali <aymuhammedali00@gmail.com>
2023-10-01 01:26:10 +0300

    updata : NewCarThree.vue

--- src/views/admin /vehicle management/NewCarThree.vue
+++ src/views/admin /vehicle management/NewCarThree.vue
@@ -22,30 +22,30 @@
       <div class="m-auto flex w-3/4 items-center justify-center">
         <div class="flex items-center justify-between">
           <div class="pr-2">
-            <img src="/public/Frame178(1).svg">
+            <img src="/public-src/genel/Frame178(1).svg">
             <p class="mt-2 text-primary">Araç</p>
           </div>
-          <img class="mb-4" src="/public/Line43(1).svg">
+          <img class="mb-4" src="/public-src/genel/Line43(1).svg">
           <div class="pl-2">
-            <img src="/public/Frame178(1).svg">
+            <img src="/public-src/genel/Frame178(1).svg">
             <p class="mt-2 text-primary"> Sürücü</p>
           </div>
-          <img class="mb-4" src="/public/Line43.svg">
+          <img class="mb-4" src="/public-src/genel/Line43.svg">
           <div class="pl-2">
-            <img src="/public/Frame180(1).svg">
+            <img src="/public-src/genel/Frame180(1).svg">
             <p class="mt-2 text-Dark800">Diğer</p>
           </div>
         </div>
       </div>
       <!-- main -->
-      <div class="m-auto mt-16 flex w-11/12 items-center justify-between ">
+      <div class="m-auto mt-16 flex w-8/12 items-center justify-between ">
         <div class="ml-16 w-full ">
           <div class="mb-16">
             <h4 class="flex items-center font-bold leading-5 text-primary">MUAYENE BİLGİLERİ
               <img class="pl-5" src="/public-src/Kullanıcı/Line48.svg"></h4>
             <div class="mb-6 mt-5">
               <p class="pl-5 text-Dark ">Yaklaşan Muayene Tarihi*</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
                 <div class="flex items-center ">
                   <p class="pl-10 pt-2"> 34 HYD 1234 </p>
                 </div>
@@ -53,7 +53,7 @@
             </div>
             <div class="mt-6">
               <p class="pl-5 text-Dark">Son Muayene Tarihi*</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
                 <div class="flex items-center ">
                   <p class="pl-10 pt-2"> 19.05.2024 </p>
                 </div>
@@ -61,7 +61,7 @@
             </div>
             <div class="mt-6">
               <p class="pl-5 text-Dark">Muayene Ücreti</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
                 <div class="flex items-center ">
                   <p class="pl-10 pt-2">₺475,00</p>
                 </div>
@@ -74,7 +74,7 @@
               <img class="pl-5" src="/public-src/Kullanıcı/Line48.svg"></h4>
             <div class="mt-6">
               <p class="pl-5 text-Dark">Yaklaşan Ödeme Tarihi*</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
                 <div class="flex items-center ">
                   <p class="pl-10 pt-2">13.12.2023</p>
                 </div>
@@ -82,7 +82,7 @@
             </div>
             <div class="mt-6">
               <p class="pl-5 text-Dark"> Son Ödeme Tarihi*</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
                 <div class="flex items-center ">
                   <p class="pl-10 pt-2">13.09.2023</p>
                 </div>
@@ -90,7 +90,7 @@
             </div>
             <div class="mt-6">
               <p class="pl-5 text-Dark">Son Ödenen Ücret</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
                 <div class="flex items-center ">
                   <p class="pl-10 pt-2"> ₺1.661,50</p>
                 </div>
@@ -102,7 +102,7 @@
               <img class="pl-5" src="/public-src/Kullanıcı/Line48.svg"></h4>
             <div class="mt-6">
               <p class="pl-5 text-Dark">Servisler </p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
                 <div class="flex items-center ">
                   <p class="pl-10 pt-2"> Giray Yetkili Servis </p>
                 </div>
@@ -110,7 +110,7 @@
             </div>
             <div class="mt-6">
               <p class="pl-5 text-Dark">Adres</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
                 <div class="flex items-center ">
                   <p class="pl-10 pt-2">Garyberli Mah. 12 Şubat İlçesi </p>
                 </div>
@@ -118,7 +118,7 @@
             </div>
             <div class="mt-6">
               <p class="pl-5 text-Dark">Yetkili</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
                 <div class="flex items-center ">
                   <p class="pl-10 pt-2">Damla Hanım</p>
                 </div>
@@ -126,7 +126,7 @@
             </div>
             <div class="mt-6">
               <p class="pl-5 text-Dark">Telefon</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter ">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker ">
                 <div class="flex items-center ">
                   <p class="pl-10 pt-2">0344 236 16 25</p>
                 </div>
@@ -140,45 +140,45 @@
               <img class="pl-5" src="/public-src/Kullanıcı/Line48.svg"></h4>
             <div class="mt-6">
               <p class="pl-5 text-Dark">Yaklaşan Sigorta Tarihi*</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
                 <p class="pl-10 pt-2">13.09.2023</p>
               </div>
             </div>
             <div class="mt-6">
               <p class="pl-5 text-Dark">Son Sigorta Tarihi*</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
                 <p class="pl-10 pt-2">13.09.2023</p>
               </div>
             </div>
 
             <div class="mt-6">
               <p class="pl-5 text-Dark">Sigorta Ücreti*</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
                 <p class="pl-10 pt-2">₺2.427,02</p>
               </div>
             </div>
 
             <div class="mt-6">
               <p class="pl-5 text-Dark">Trafik Sigortası*</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter pl-10 pt-2">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker pl-10 pt-2">
                 <a class="font-medium text-blue-600  hover:underline" href="#">https://www.resmigazete.gov.tr/.pdf</a>
               </div>
             </div>
             <div class="mt-6">
               <p class="pl-5 text-Dark">Kasko Sigortası*</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter pl-10 pt-2">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker  pl-10 pt-2">
                 <a class="font-medium text-blue-600  hover:underline" href="#">https://www.resmigazete.gov.tr/.pdf</a>
               </div>
             </div>
             <div class="mt-6">
               <p class="pl-5 text-Dark">Trafik Sigorta Adı*</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker ">
                 <p class="pl-10 pt-2">DOĞA SİGORTA</p>
               </div>
             </div>
             <div class="mt-6">
               <p class="pl-5 text-Dark">Acenta Adı*</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker ">
                 <p class="pl-10 pt-2">104.000 KM</p>
               </div>
             </div>
@@ -188,20 +188,20 @@
               <img class="pl-5" src="/public-src/Kullanıcı/Line48.svg"></h4>
             <div class="mt-6">
               <p class="pl-5 text-Dark">Güncel Fiyat Bakılan Tarih</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker  ">
                 <p class="pl-10 pt-2">13.09.2023</p>
               </div>
             </div>
             <div class="mt-6">
               <p class="pl-5 text-Dark">Araç Güncel Değer</p>
-              <div class="h-10 w-3/4 rounded-xl bg-GrayLighter">
+              <div class="h-10 w-3/4 rounded-xl border border-Graydarker ">
                 <p class="pl-10 pt-2"> ₺ 465.900</p>
               </div>
             </div>
 
             <div class="mt-6">
               <p class="pl-5 text-Dark">Alınan Bilgi Linki</p>
-              <div class="h-[115px] w-3/4 rounded-xl bg-GrayLighter p-6 ">
+              <div class="h-[115px] w-3/4 rounded-xl border border-Graydarker p-6 ">
                 <a class="font-medium text-blue-600  hover:underline" href="#">https://www.sahibinden.com/ilan/vasita-otomobil-opel-2008-model-opel-corsa-1.3-cdtit-1124414828/detay</a>
               </div>
             </div>
@@ -213,14 +213,14 @@
           <button
               class="mb-2 mr-2 flex items-center rounded-lg bg-PrimaryLight px-5 py-2.5 text-sm font-medium text-primary  focus:outline-none focus:ring-2 focus:ring-primary "
               type="button">
-            <img class="pr-2 text-white" src="/public/LeftRounded.svg">
+            <img class="pr-2 text-white" src="/public-src/genel/LeftRounded.svg">
             <router-link to="newcartwo">Önceki</router-link>
           </button>
           <button
               class="mb-2 mr-2 flex items-center rounded-lg bg-primary px-5 py-2.5 text-sm font-medium text-white  focus:outline-none focus:ring-2 focus:ring-primary "
               type="button">
             Tamamla
-            <img class="pl-2 text-white" src="/public/Frame(21).svg">
+            <img class="pl-2 text-white" src="/public-src/genel/Frame(21).svg">
           </button>
 
         </div>
