8ddb52a90a4b7cf2992218d2969133180d8d34f9
ayildizali <aymuhammedali00@gmail.com>
2023-10-04 13:07:21 +0300

    updata : EditSpending.vue

--- src/views/admin /expenses/EditSpending.vue
+++ src/views/admin /expenses/EditSpending.vue
@@ -4,9 +4,11 @@
       <!-- header -->
       <div class="m-auto flex h-32 w-3/4 items-center justify-between">
         <div class="ml-5 flex w-3/4 items-center">
-          <img class="mr-5" src="/public-src/Kullanıcı/Frame137.svg">
+          <router-link to="/otherexpenses"><img class="mr-5" src="/public-src/Kullanıcı/Frame137.svg"></router-link>
+
           <h2 class="text-4xl font-bold leading-9 text-green-800">
-            Diğer Harcamalar
+            <router-link to="/otherexpenses">Diğer Harcamalar</router-link>
+
           </h2>
           <h4 class="ml-8 text-base font-medium text-[#717982]"> / Düzenle </h4>
         </div>
@@ -22,62 +24,80 @@
       <div class=" m-auto mt-16 flex w-3/4 items-center justify-between">
         <div class="ml-32 mt-0  w-full">
           <div class=" ">
-            <p class="pl-5 text-Dark"> Tarih</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker  ">
-              <div class="flex items-center ">
-                <p class="pl-10 pt-2"></p>
-              </div>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Tarih"
+                placeholder="     Tarih Giriniz..."
+                rowclass=""
+                type="date"
+            />
           </div>
           <div class="mt-6">
-            <p class="pl-5 text-Dark">Plaka</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker ">
-              <div class="flex items-center ">
-                <p class="pl-10 pt-2"></p>
-              </div>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Plaka"
+                placeholder="     Plaka Giriniz..."
+                rowclass=""
+            />
           </div>
           <div class="mt-6">
-            <p class="pl-5 text-Dark">Araç Kullnıcısı</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
-              <div class="flex items-center ">
-                <p class="pl-10 pt-2"></p>
-              </div>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Araç Kullanıcısı"
+                placeholder="     Araç Kullanıcısı Giriniz..."
+                rowclass=""
+            />
           </div>
           <div class="mt-6">
-            <p class="pl-5 text-Dark">İl </p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker ">
-              <div class="flex items-center ">
-                <p class="pl-10 pt-2"></p>
-              </div>
-            </div>
+
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="İL"
+                placeholder="    İL Giriniz..."
+                rowclass=""
+            />
           </div>
         </div>
-        <div class="w-full ">
+        <div class="mt-14 w-full ">
           <div class="">
-            <p class="pl-5 text-Dark">Fatura</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
-              <p class="pl-10 pt-2"></p>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Fatura"
+                placeholder="     Fatura Giriniz..."
+                rowclass=""
+            />
           </div>
           <div class="mt-6">
-            <p class="pl-5 text-Dark">Harcama Türü</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
-              <p class="pl-10 pt-2"></p>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Harcama Türü"
+                placeholder="     Harcama Türü Giriniz..."
+                rowclass=""
+            />
           </div>
           <div class="mt-6">
-            <p class="pl-5 text-Dark">Fatura Tutarı</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
-              <p class="pl-10 pt-2"></p>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Fatura Tutarı"
+                placeholder="     Fatura Tutarı Giriniz..."
+                rowclass=""
+            />
           </div>
           <div class="mt-6">
-            <p class="pl-5 text-Dark">Açıklama</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
-              <p class="pl-10 pt-2"></p>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass="h-24 border-2 border-Graydarker w-[370px] rounded-xl"
+                label="Açiklama"
+                placeholder="     Açıklama Giriniz..."
+                rowclass=""
+            />
           </div>
         </div>
       </div>
@@ -86,7 +106,7 @@
           <button
               class="mb-2 mr-2 rounded-lg bg-primary px-5 py-2.5 text-sm font-medium text-white  focus:outline-none focus:ring-2 focus:ring-primary "
               type="button">
-            Düzenle
+            <router-link to="/otherexpenses"> Düzenle</router-link>
           </button>
         </div>
       </div>
@@ -95,12 +115,9 @@
 </template>
 
 <script>
-import {defineComponent} from "vue";
+import inputTag from "../../../components/custom/input.tag.vue";
 
-export default defineComponent(
-    {
-      name: "EditSpanding",
-      components: ""
-    }
-)
+export default {
+  components: {inputTag}
+}
 </script>
