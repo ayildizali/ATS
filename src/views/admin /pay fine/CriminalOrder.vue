43a67df25faba46af26de194d480f7b0f96dfad7
ayildizali <aymuhammedali00@gmail.com>
2023-10-04 13:07:01 +0300

    updata : CriminalOrder.vue

--- src/views/admin /pay fine/CriminalOrder.vue
+++ src/views/admin /pay fine/CriminalOrder.vue
@@ -4,13 +4,12 @@
       <!-- header -->
       <div class="m-auto flex h-32 w-3/4 items-center justify-between">
         <div class="ml-5 flex w-3/4 items-center">
-          <img class="mr-5" src="/public-src/Kullanıcı/Frame137.svg">
+          <router-link to="/payfine"><img class="mr-5" src="/public-src/Kullanıcı/Frame137.svg"></router-link>
           <h2 class=" text-4xl font-bold leading-9 text-green-800">
-            Ceza Ödeme
+            <router-link to="/payfine">Ceza Ödeme</router-link>
           </h2>
           <h4 class="ml-8 text-base font-medium text-[#717982]"> / Ceza Düzenle </h4>
         </div>
-
         <div class="flex items-center">
           <nav class="pr-3">
             <img src="/public-src/Kullanıcı/Frame(12).svg">
@@ -24,115 +23,145 @@
       <div class=" m-auto mt-16 flex w-3/4 items-center justify-between">
         <div class="ml-32 w-full">
           <div class=" ">
-            <p class="text-Dark">Tebliğ Tarihi*</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker  ">
-              <div class="flex items-center ">
-                <p class="pl-10 pt-2"></p>
-              </div>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Tebliğ Tarihi"
+                placeholder="     Tebliğ Tarihi Giriniz..."
+                rowclass=""
+                type="date"
+            />
           </div>
           <div class="mt-6">
-            <p class="text-Dark">Evrak No</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker ">
-              <div class="flex items-center ">
-                <p class="pl-10 pt-2"></p>
-              </div>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Evrak No"
+                placeholder="     Evrak No Giriniz..."
+                rowclass=""
+            />
           </div>
           <div class="mt-6">
-            <p class="text-Dark">Plaka</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
-              <div class="flex items-center ">
-                <p class="pl-10 pt-2"></p>
-              </div>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Plaka"
+                placeholder="     Plaka Giriniz..."
+                rowclass=""
+            />
           </div>
           <div class="mt-6">
-            <p class="text-Dark">Ceza Türü </p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker ">
-              <div class="flex items-center ">
-                <p class="pl-10 pt-2"></p>
-              </div>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Ceza Türü"
+                placeholder="     Ceza Türü Giriniz..."
+                rowclass=""
+            />
           </div>
           <div class="mt-6">
-            <p class="text-Dark">erken ödeme tarihi</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker ">
-              <div class="flex items-center ">
-                <p class="pl-10 pt-2"></p>
-              </div>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Erken Ödeme Tarihi"
+                placeholder="     Erken ödeme Tarihi Giriniz..."
+                rowclass=""
+                type="date"
+            />
           </div>
           <div class="mt-6">
-            <p class="text-Dark">ödeme tarihi</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker ">
-              <div class="flex items-center ">
-                <p class="pl-10 pt-2"></p>
-              </div>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Ödeme Tarihi"
+                placeholder="     Ödeme Tarihi Giriniz..."
+                rowclass=""
+                type="date"
+            />
           </div>
           <div class="mt-6">
-            <p class="text-Dark">dekont</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker ">
-              <div class="flex items-center ">
-                <p class="pl-10 pt-2"></p>
-              </div>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Dekont"
+                placeholder="     Dekont Giriniz..."
+                rowclass=""
+            />
           </div>
         </div>
         <div class="w-full ">
           <div class="">
-            <p class="text-Dark">Tebliğ Türü</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
-              <p class="pl-10 pt-2"></p>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Tebliği Türü"
+                placeholder="     Tebliğ Türü Giriniz..."
+                rowclass=""
+            />
           </div>
           <div class="mt-6">
-            <p class="text-Dark">Türü</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
-              <p class="pl-10 pt-2"></p>
-            </div>
-          </div>
-
-          <div class="mt-6">
-            <p class="text-Dark">Borçlu</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
-              <p class="pl-10 pt-2"></p>
-            </div>
-          </div>
-
-          <div class="mt-6">
-            <p class="text-Dark">Ceza Tutarı</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
-              <p class="pl-10 pt-2"></p>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Türü"
+                placeholder="     Türü Giriniz..."
+                rowclass=""
+            />
           </div>
           <div class="mt-6">
-            <p class="text-Dark">Erken Ödeme Karı</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker bg-Graydarker">
-              <p class="pl-10 pt-2"></p>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Borçlu"
+                placeholder="     Borçlu Giriniz..."
+                rowclass=""
+            />
           </div>
           <div class="mt-6">
-            <p class="text-Dark">Ödeme Yeri</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
-              <p class="pl-10 pt-2"></p>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Ceza Ödeme Tutarı"
+                placeholder="     Ceza Ödeme Tutarı Giriniz..."
+                rowclass=""
+            />
           </div>
           <div class="mt-6">
-            <p class="text-Dark">Ödeme Durumu</p>
-            <div class="h-10 w-3/4 rounded-xl border border-Graydarker">
-              <p class="pl-10 pt-2"></p>
-            </div>
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Erken Ödeme Karı"
+                placeholder="     Erken Ödeme Karı Giriniz..."
+                rowclass=""
+            />
+          </div>
+          <div class="mt-6">
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Ödeme Yeri"
+                placeholder="     Ödeme Yeri Giriniz..."
+                rowclass=""
+            />
+          </div>
+          <div class="mt-6">
+            <input-tag
+                :required="true"
+                inputclass=""
+                label="Ödeme Durumu"
+                placeholder="     Ödeme Durumu Giriniz..."
+                rowclass=""
+            />
           </div>
         </div>
-
       </div>
       <div class="mt-14 flex w-full items-center justify-center">
         <div class=" mr-44 flex w-2/3 items-center justify-end">
-          <button class="mb-2 mr-2 rounded-lg bg-primary px-5 py-2.5 text-sm font-medium text-white  focus:outline-none focus:ring-2 focus:ring-primary "
-                  type="button">
-            Düzenle
+          <button
+              class="mb-2 mr-2 rounded-lg bg-primary px-5 py-2.5 text-sm font-medium text-white  focus:outline-none focus:ring-2 focus:ring-primary "
+              type="button">
+            <router-link to="/payfine">Düzenle</router-link>
+
           </button>
         </div>
       </div>
@@ -141,12 +170,9 @@
 </template>
 
 <script>
-import {defineComponent} from "vue";
+import inputTag from "../../../components/custom/input.tag.vue";
 
-export default defineComponent(
-    {
-      name: "CriminalOrder",
-      components: ""
-    }
-)
+export default {
+  components: {inputTag}
+}
 </script>
