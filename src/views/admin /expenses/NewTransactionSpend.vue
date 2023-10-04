85d2cd018ffc885cb9d72d133dea592a3f6f0e53
ayildizali <aymuhammedali00@gmail.com>
2023-10-04 13:08:54 +0300

    updata : NewTransactionSpend.vue

--- src/views/admin /expenses/NewTransactionSpend.vue
+++ src/views/admin /expenses/NewTransactionSpend.vue
@@ -4,11 +4,13 @@
       <!-- header -->
       <div class="m-auto flex h-32 w-3/4 items-center justify-between">
         <div class="ml-5 flex w-3/4 items-center">
-          <img class="mr-5" src="/public-src/Kullanıcı/Frame137.svg">
+          <router-link to="/otherexpenses"><img class="mr-5" src="/public-src/Kullanıcı/Frame137.svg"></router-link>
+
           <h2 class="text-4xl font-bold leading-9 text-green-800">
-            Diğer Harcamalar
+            <router-link to="/otherexpenses">Diğer Harcamalar</router-link>
+
           </h2>
-          <h4 class="ml-8 text-base font-medium text-[#717982]"> / Yeni İşlem </h4>
+          <h4 class="ml-8 text-base font-medium text-Dark800"> / Yeni İşlem </h4>
         </div>
 
         <div class="flex items-center">
@@ -107,7 +109,8 @@
           <button
               class="mb-2 mr-2 rounded-lg bg-primary px-5 py-2.5 text-sm font-medium text-white  focus:outline-none focus:ring-2 focus:ring-primary "
               type="button">
-            Tamamla
+            <router-link to="/otherexpenses">Tamamla</router-link>
+
           </button>
         </div>
       </div>
