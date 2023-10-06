5f80cf693324f5d5bc8411ccfc2a75557ae7e7c5
ayildizali <aymuhammedali00@gmail.com>
2023-10-06 08:42:24 +0300

    updata : main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './index.css'
+
+
 // @ts-ignore
 import router from "./router/index.js"
+
+
 // @ts-ignore
 import App from './App.vue'
+
+
 createApp(App).use(router).mount('#app')
