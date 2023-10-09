936b4baa6cf731c2e1078e8a309a1caaceb15f55
ayildizali <aymuhammedali00@gmail.com>
2023-10-09 16:52:23 +0300

    updata : CarManagement.vue

--- src/views/admin /vehicle management/CarManagement.vue
+++ src/views/admin /vehicle management/CarManagement.vue
@@ -1,8 +1,73 @@
 <template>
   <div class=" flex h-full w-full items-center justify-between">
+    <popup-modal ref="aracsatis">
+      <div class=" z-50 h-[700px] w-[530px] ">
+        <router-link to="/management">
+          <div class="flex w-[520px] items-center justify-end">
+            <img src="/public/Frame(200).svg">
+          </div>
+        </router-link>
+        <h1 class=" mt-3 flex items-center justify-center text-3xl font-bold text-primary">Araç Satış</h1>
+        <div class=" ml-9 mt-7 ">
+          <div class="mt-4">
+            <input-tag
+                :required="true"
+                inputclass="w-[450px] h-[40px] border-2 border-Dark800 rounded-lg "
+                label="Plaka"
+                placeholder="    Plaka Giriniz..."
+                rowclass=""
+            />
+          </div>
+          <div class="mt-4">
+            <input-tag
+                :required="true"
+                inputclass="w-[450px] h-[40px] border-2 border-Dark800 rounded-lg "
+                label="Son Km Durumu "
+                placeholder="   Son Km Durumu Giriniz..."
+                rowclass=""
+            />
+          </div>
+          <div class="mt-4">
+            <input-tag
+                :required="true"
+                inputclass="w-[450px] h-[40px] border-2 border-Dark800 rounded-lg "
+                label="Araç Satış Tarihi"
+                placeholder="    Araç Satış Tarihi Giriniz..."
+                rowclass=""
+            />
+          </div>
+          <div class="mt-4">
+            <input-tag
+                :required="true"
+                inputclass="w-[450px] h-[40px] border-2 border-Dark800 rounded-lg"
+                label="Araç Satış Tutarı"
+                placeholder="    Araç Satış Tutarı Giriniz..."
+                rowclass=""
+            />
+          </div>
+          <div class="mt-4">
+            <input-tag
+                :required="true"
+                inputclass="w-[450px] h-[135px] border-2 border-Dark800 rounded-lg"
+                label="Açıklama"
+                placeholder="    Açıklama Giriniz..."
+                rowclass=""
+            />
+          </div>
+        </div>
+        <router-link to="/management">
+          <button class=" ml-9 mt-10 h-[37px] w-[450px] rounded-lg border-2 border-primary bg-primary text-white ">
+            Tamamla
+          </button>
+        </router-link>
+
+      </div>
+    </popup-modal>
     <div class="flex-1">
       <!-- header -->
+
       <div class="m-auto flex h-32 w-3/4 items-center justify-between">
+
         <div class="ml-5 flex w-3/4 items-center">
           <img class="mr-5" src="/public-src/Kullanıcı/Frame137.svg">
           <h2 class="text-4xl font-bold leading-9 text-green-800">
@@ -19,7 +84,6 @@
             <router-link to="/vehiclesold">Satılan Araç</router-link>
           </button>
         </div>
-
         <div class="flex items-center">
           <nav class="pr-3">
             <img src="/public-src/Kullanıcı/Frame(12).svg">
@@ -28,8 +92,8 @@
         </div>
       </div>
       <!-- main -->
-      <div class="relative m-auto mt-16  flex w-3/4 items-center overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-left text-sm text-GrayLightest">
+      <div class="relative  m-auto  mt-16 flex w-3/4 items-center overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="z-40 w-full text-left text-sm text-GrayLightest">
           <thead class="bg-GrayLightest text-xs uppercase text-Dark900">
           <tr>
             <th class="px-6 py-3" scope="col">
@@ -75,11 +139,14 @@
           </thead>
           <tbody>
           <tr class="border-b bg-white ">
-            <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
-              <div class="m-auto flex w-20 items-center">
-                <img src="/public-src/admin/Frame(20).svg">
-                <img class="pl-2" src="/public-src/admin/Frame(17).svg">
-                <img class="pl-2" src="/public-src/admin/Frame(14).svg">
+            <th class="whitespace-nowrap px-2 py-1 font-medium text-Dark900" scope="row">
+              <div class="m-auto flex w-24 items-center">
+                <img class="pl-2" src="/public-src/admin/Frame(20).svg">
+                <router-link class="pl-2" to="/editvahiclemaneg"><img
+                    src="/public-src/admin/Frame(17).svg">
+                </router-link>
+
+                <img class="pl-2" src="/public-src/admin/Frame(14).svg" @click="asdasd()">
               </div>
             </th>
             <td class="px-6 py-4 text-Dark900">
@@ -121,10 +188,12 @@
           </tr>
           <tr class="border-b bg-gray-50 text-Graydarker">
             <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900 " scope="row">
-              <div class="m-auto flex w-20 items-center">
-                <img src="/public-src/admin/Frame(20).svg">
-                <img class="pl-2" src="/public-src/admin/Frame(17).svg">
-                <img class="pl-2" src="/public-src/admin/Frame(14).svg">
+              <div class="m-auto flex w-24 items-center">
+                <img class="pl-2" src="/public-src/admin/Frame(20).svg">
+                <router-link class="pl-2" to="/editvahiclemaneg"><img
+                    src="/public-src/admin/Frame(17).svg">
+                </router-link>
+                <img class="pl-2" src="/public-src/admin/Frame(14).svg " @click="asdasd()">
               </div>
             </th>
             <td class="px-6 py-4 text-Dark900">
@@ -166,10 +235,12 @@
           </tr>
           <tr class="border-b bg-white ">
             <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
-              <div class="m-auto flex w-20 items-center">
-                <img src="/public-src/admin/Frame(20).svg">
-                <img class="pl-2" src="/public-src/admin/Frame(17).svg">
-                <img class="pl-2" src="/public-src/admin/Frame(14).svg">
+              <div class="m-auto flex w-24 items-center">
+                <img class="pl-2" src="/public-src/admin/Frame(20).svg">
+                <router-link class="pl-2" to="/editvahiclemaneg"><img
+                    src="/public-src/admin/Frame(17).svg">
+                </router-link>
+                <img class="pl-2" src="/public-src/admin/Frame(14).svg" @click="asdasd()">
               </div>
             </th>
             <td class="px-6 py-4 text-Dark900">
@@ -211,10 +282,12 @@
           </tr>
           <tr class="border-b bg-gray-50 text-Graydarker">
             <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900 " scope="row">
-              <div class="m-auto flex w-20 items-center">
-                <img src="/public-src/admin/Frame(20).svg">
-                <img class="pl-2" src="/public-src/admin/Frame(17).svg">
-                <img class="pl-2" src="/public-src/admin/Frame(14).svg">
+              <div class="m-auto flex w-24 items-center">
+                <img class="pl-2" src="/public-src/admin/Frame(20).svg">
+                <router-link class="pl-2" to="/editvahiclemaneg"><img
+                    src="/public-src/admin/Frame(17).svg">
+                </router-link>
+                <img class="pl-2" src="/public-src/admin/Frame(14).svg" @click="asdasd()">
               </div>
             </th>
             <td class="px-6 py-4 text-Dark900">
@@ -256,10 +329,12 @@
           </tr>
           <tr class="border-b bg-white ">
             <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
-              <div class="m-auto flex w-20 items-center">
-                <img src="/public-src/admin/Frame(20).svg">
-                <img class="pl-2" src="/public-src/admin/Frame(17).svg">
-                <img class="pl-2" src="/public-src/admin/Frame(14).svg">
+              <div class="m-auto flex w-24 items-center">
+                <img class="pl-2" src="/public-src/admin/Frame(20).svg">
+                <router-link class="pl-2" to="/editvahiclemaneg"><img
+                    src="/public-src/admin/Frame(17).svg">
+                </router-link>
+                <img class="pl-2" src="/public-src/admin/Frame(14).svg" @click="asdasd()">
               </div>
             </th>
             <td class="px-6 py-4 text-Dark900">
@@ -302,10 +377,12 @@
           </tr>
           <tr class="border-b bg-gray-50 text-Graydarker">
             <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900 " scope="row">
-              <div class="m-auto flex w-20 items-center">
-                <img src="/public-src/admin/Frame(20).svg">
-                <img class="pl-2" src="/public-src/admin/Frame(17).svg">
-                <img class="pl-2" src="/public-src/admin/Frame(14).svg">
+              <div class="m-auto flex w-24 items-center">
+                <img class="pl-2" src="/public-src/admin/Frame(20).svg">
+                <router-link class="pl-2" to="/editvahiclemaneg"><img
+                    src="/public-src/admin/Frame(17).svg">
+                </router-link>
+                <img class="pl-2" src="/public-src/admin/Frame(14).svg" @click="asdasd()">
               </div>
             </th>
             <td class="px-6 py-4 text-Dark900">
@@ -347,10 +424,12 @@
           </tr>
           <tr class="border-b bg-white ">
             <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900" scope="row">
-              <div class="m-auto flex w-20 items-center">
-                <img src="/public-src/admin/Frame(20).svg">
-                <img class="pl-2" src="/public-src/admin/Frame(17).svg">
-                <img class="pl-2" src="/public-src/admin/Frame(14).svg">
+              <div class="m-auto flex w-24 items-center">
+                <img class="pl-2" src="/public-src/admin/Frame(20).svg">
+                <router-link class="pl-2" to="/editvahiclemaneg"><img
+                    src="/public-src/admin/Frame(17).svg">
+                </router-link>
+                <img class="pl-2" src="/public-src/admin/Frame(14).svg" @click="asdasd()">
               </div>
             </th>
             <td class="px-6 py-4 text-Dark900">
@@ -393,9 +472,11 @@
           </tr>
           <tr class="border-b bg-gray-50 text-Graydarker">
             <th class="whitespace-nowrap px-6 py-4 font-medium text-Dark900 " scope="row">
-              <div class="m-auto flex w-20 items-center">
-                <img src="/public-src/admin/Frame(20).svg">
-                <img class="pl-2" src="/public-src/admin/Frame(17).svg">
+              <div class="m-auto flex w-24 items-center">
+                <img class="pl-2" src="/public-src/admin/Frame(20).svg">
+                <router-link class="pl-2" to="/editvahiclemaneg"><img
+                    src="/public-src/admin/Frame(17).svg">
+                </router-link>
                 <img class="pl-2" src="/public-src/admin/Frame(14).svg">
               </div>
             </th>
@@ -452,17 +533,27 @@
   </div>
 </template>
 <script>
+import PopupModal from "../../../components/PopupModal.vue" // import inputTag from "../../../components/custom/input.tag.vue";
 import inputTag from "../../../components/custom/input.tag.vue";
 
 export default {
-  name: "MoDel",
+
+  components: {PopupModal, inputTag},
   data() {
     return {
-      toggleModel: false
-    };
+      aracsatis: null
+    }
   },
-  methods: {},
-  components: {inputTag}
+  methods: {
+    asdasd() {
+      this.$refs.aracsatis.openModal()
+    }
+  },
 
 };
 </script>
+<style scoped>
+input:focus {
+  outline: none;
+}
+</style>
