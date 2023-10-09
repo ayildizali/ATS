e2dbcd05d36e70a6cdab183b258f899cf27ead57
ayildizali <aymuhammedali00@gmail.com>
2023-10-09 16:53:25 +0300

    updata : PopupModal.vue

---
+++ src/components/PopupModal.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="show" class="absolute left-0 top-0 flex h-full w-full justify-center bg-black bg-opacity-30 px-8">
+    <div class="mt-32 self-start rounded-lg bg-white p-4">
+      <slot/>
+    </div>
+  </div>
+
+</template>
+<script>
+import {ref} from "vue";
+
+export default {
+  setup() {
+    const show = ref(false)
+    const openModal = () => {
+      show.value = true
+    }
+    const closeModal = () => {
+      show.value = false
+    }
+    return {
+      show,
+      closeModal,
+      openModal,
+    }
+  }
+}
+
+</script>
+<style scoped>
+
+</style>
