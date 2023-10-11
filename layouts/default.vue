a215c12d651e494e800bd0a09e5abff68718eea5
ayildizali <aymuhammedali00@gmail.com>
2023-10-11 09:12:59 +0300

    new :defoult.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex">
-        <side-bar/>
-        <router-view/>
-    </div>
+  <div class="flex w-full">
+    <side-bar/>
+    <router-view class="shrink"/>
+  </div>
 </template>
 <script setup>
 import SideBar from "../src/components/SideBar.vue"
